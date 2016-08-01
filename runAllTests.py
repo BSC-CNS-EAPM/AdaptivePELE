e62923c64fbcdf24ef79d1403b326c18560bc81b
D. Lecina <danilecina@gmail.com>
2016-08-01 17:18:32 +0200

    Added testClustering and refactored clustering.py

--- runAllTests.py
+++ runAllTests.py
@@ -1,12 +1,14 @@
 import unittest
 import tests.testStartingConformationsCalculator as tStartConf
 import tests.testatomset as tatomset
+import tests.testClustering as tClustering
 
 def main():
     testSuite = unittest.TestSuite()
 
     #testSuite.addTest(unittest.makeSuite(tStartConf.TestStartingConformationsCalculator))
-    testSuite.addTest(unittest.makeSuite(tatomset.atomsetTest))
+    #testSuite.addTest(unittest.makeSuite(tatomset.atomsetTest))
+    testSuite.addTest(unittest.makeSuite(tClustering.clusteringTest))
 
     runner = unittest.TextTestRunner()
     runner.run(testSuite)
