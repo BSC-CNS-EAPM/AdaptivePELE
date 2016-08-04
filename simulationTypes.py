ca0d26ea8ca214c9a97942d847a765894e59ccb5
D. Lecina <danilecina@gmail.com>
2016-08-04 20:32:05 +0200

    Refactored names in order to unify criteria

--- simulationTypes.py
+++ simulationTypes.py
@@ -4,7 +4,7 @@ class SIMULATION_TYPE:
     PELE, MD, TEST = range(3)
 
 SIMULATION_TYPE_TO_STRING_DICTIONARY = {
-    SIMULATION_TYPE.PELE:blockNames.SIMULATION_TYPE.PELE,
-    SIMULATION_TYPE.MD:blockNames.SIMULATION_TYPE.MD,
-    SIMULATION_TYPE.TEST:blockNames.SIMULATION_TYPE.TEST
+    SIMULATION_TYPE.PELE:blockNames.SimulationType.pele,
+    SIMULATION_TYPE.MD:blockNames.SimulationType.md,
+    SIMULATION_TYPE.TEST:blockNames.SimulationType.test
 }
