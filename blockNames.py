0e5a4ee3d854ae74aabb8f0e47fe19df713f8638
(D. Lecina) <(danilecina@gmail.com)>
2016-08-04 17:33:23 +0200

    Refactor spawning and simulation types

--- blockNames.py
+++ blockNames.py
@@ -1,10 +1,12 @@
 class STRING_SPAWNING_TYPES:
+    type = 'type'
     sameWeight = "sameWeight"
     inverselyProportional = "inverselyProportional"
     epsilon = "epsilon"
     FAST = "FAST"
     
 class SPAWNING_PARAMS:
+    params = "params"
     EPSILON = "epsilon"
     TEMPERATURE = "T"
     THRESHOLD = "threshold"
@@ -34,20 +36,7 @@ class SIMULATION_PARAMS:
     iterations = "iterations"
 
 """ MOVE TO CONSTANTS """
-import startingConformationsCalculator
 import SimulationRunner
 
-SIMULATION_TYPE_TO_STRING_DICTIONARY = {
-    SimulationRunner.SIMULATION_TYPE.PELE:SIMULATION_TYPE.PELE,
-    SimulationRunner.SIMULATION_TYPE.MD:SIMULATION_TYPE.MD,
-    SimulationRunner.SIMULATION_TYPE.TEST:SIMULATION_TYPE.TEST
-    }
-
-SPAWNING_TYPE_TO_STRING_DICTIONARY = {
-    startingConformationsCalculator.SPAWNING_TYPES.sameWeight:STRING_SPAWNING_TYPES.sameWeight, 
-    startingConformationsCalculator.SPAWNING_TYPES.inverselyProportional:STRING_SPAWNING_TYPES.inverselyProportional, 
-    startingConformationsCalculator.SPAWNING_TYPES.epsilon:STRING_SPAWNING_TYPES.epsilon,
-    startingConformationsCalculator.SPAWNING_TYPES.FAST:STRING_SPAWNING_TYPES.FAST
-}
 
 """ END MOVE TO CONSTANTS """
