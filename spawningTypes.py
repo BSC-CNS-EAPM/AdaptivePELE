a27e8de7f01e8ccd5c76093ff0927ffebdb4c307
D. Lecina <danilecina@gmail.com>
2016-08-05 09:44:53 +0200

    Changed RESTART parameter for restart. Also changed FAST for fast in spawningTypes, in order to match with the blockNames file

--- spawningTypes.py
+++ spawningTypes.py
@@ -7,5 +7,5 @@ SPAWNING_TYPE_TO_STRING_DICTIONARY = {
     SPAWNING_TYPES.sameWeight:blockNames.StringSpawningTypes.sameWeight, 
     SPAWNING_TYPES.inverselyProportional:blockNames.StringSpawningTypes.inverselyProportional, 
     SPAWNING_TYPES.epsilon:blockNames.StringSpawningTypes.epsilon,
-    SPAWNING_TYPES.FAST:blockNames.StringSpawningTypes.FAST
+    SPAWNING_TYPES.FAST:blockNames.StringSpawningTypes.fast
 }
