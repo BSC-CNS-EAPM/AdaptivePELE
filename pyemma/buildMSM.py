8ca4b9230c711bfaaeb812aa4890c11b47c2793d
(D. Lecina) <(danilecina@gmail.com)>
2016-09-02 17:04:23 +0200

    Added loop for decision in ITS+connectivity+pcca+CKtests

--- pyemma/buildMSM.py
+++ pyemma/buildMSM.py
@@ -15,6 +15,9 @@ def makeFolder(outputDir):
         os.makedirs(outputDir)
 
 def main():
+    #TODO: Define blocks with tasks to make the program more modular
+    #TODO: Add lines saying what the program is doing
+
     ### parameters
     trajectoryFolder = "test/MSM2" 
     trajectoryBasename = "*traj_*" 
@@ -25,14 +28,15 @@ def main():
     itsOutput = "its.png"
     numberOfITS = -1
     itsErrors=None #'bayes'
-
+    nsetsCK = 2
+    error_estimationCK=False
+    membershipsCK=None
+    numPCCA = 7
 
     ### constants
     discretizedFolder = "discretized"
     clusterCentersFile = os.path.join(discretizedFolder, "clusterCenters.dat")
     discTraj = os.path.join(discretizedFolder, "%s.disctraj")
-
-
 
     #program
 
@@ -53,15 +57,67 @@ def main():
     plt.show()
     """
 
-    #connectivity
-
+    plot_its = None #just a placeholder so it doesn't go out of scope
+    is_converged = False
     #its
-    its_object = msm.calculateITS(cl.dtrajs, lagtimes, itsErrors)
-    msm.plotITS(its_object, itsOutput, numberOfITS)
-
+    while not is_converged:
+        
+        its_object = msm.calculateITS(cl.dtrajs, lagtimes, itsErrors)
+        plot_its = msm.plotITS(its_object, itsOutput, numberOfITS)
+        plt.show()
+        while True:
+            convergence_answer = raw_input("Has the ITS plot converged?[y/n] ")
+            convergence_answer.rstrip()
+            convergence_answer = convergence_answer or "y" #Making yes the default
+            #answer
+            if convergence_answer.lower() == "y" or convergence_answer.lower() == "yes":
+                is_converged = True
+                lagtime_str = raw_input("Please input the lagtime to construct the MSM: ")
+                lagtime = int(lagtime_str.rstrip())
+                break
+            elif convergence_answer.lower() == "n" or convergence_answer.lower() == "no":
+                break
+            else:
+                print "Answer not valid. Please answer yes or no"
+        if not is_converged:
+            new_lagtimes = raw_input("Do you want to define new lagtimes or add to the previous?[add(a)/new(n)] ")
+            new_lagtimes.rstrip()
+            if new_lagtimes.lower() == "add" or new_lagtimes.lower() == "a":
+                lag_list = raw_input("Please input the lagtimes you want to add separated by a space: ")
+                lag_list.rstrip()
+                lagtimes.extend(map(int,lag_list.split(" ")))
+            elif new_lagtimes.lower() == "new" or new_lagtimes.lower() == "n":
+                lag_list = raw_input("Please input the new lagtimes separated by a space: ")
+                lag_list.rstrip()
+                lagtimes = map(int,lag_list.split(" "))
+            lagtimes.sort()
     #estimation
-    MSM_object = msm.estimateMSM(cl.dtrajs, 100)
+    MSM_object = msm.estimateMSM(cl.dtrajs, lagtime)
 
+    #connectivity
+    if msm.is_connected(MSM_object):
+        print "The MSM estimated is fully connected"
+    else:
+        print "The MSM estimated is not fully connected"
+        unconnected_sets = get_connected_sets(MSM_object)
+        print "The MSM estimated has %d connected sets with sizes:" % len(unconnected_sets)
+        for index, uncon_set in enumerate(unconnected_sets):
+            print "Set %d has %d elements" % (index, uncon_set.size)
+
+    #PCCA
+    MSM_object = msm.calculatePCCA(MSM_object, numPCCA)
+
+    #Chapman-Kolgomorov validation
+    #TODO: add decision-making loop for validity of CK test
+    nsetsCK = len(MSM_object.metastable_sets)
+    membershipsCK = MSM_object.metastable_memberships
+    CKObject = msm.ChapmanKolmogorovTest(MSM_object,
+                                         nsetsCK,memberships=membershipsCK,
+                                         error_estimation=error_estimationCK)
+    msm.plotChapmanKolmogorovTest(CKObject)
+    plt.show()
+
+    #TODO: TPT block
     kbt = 0.0019872041*300
     pi = MSM_object.stationary_distribution
 
@@ -85,13 +141,10 @@ def main():
 
     Gpmf = -kbt*np.log(pi/np.max(pi))
 
-    print Gpmf.shape
     Gpmf = np.expand_dims(Gpmf, axis=1)
-    print Gpmf.shape
     output = np.hstack((cl.clustercenters, Gpmf))
     np.savetxt("output.txt", output)
 
-    
 
 if __name__ == "__main__":
     main()
--- pyemma/msm.py
+++ pyemma/msm.py
@@ -8,6 +8,7 @@ def calculateITS(trajectories, lagtimes, errors = None):
 
 def plotITS(its_object, its_plot_file=None, nits=-1):
     its_plot = mplt.plot_implied_timescales(its_object, outfile=its_plot_file, nits=nits)
+    return its_plot
 
 def estimateMSM(trajectories,lagtime):
     """ Estimate a MSM from the trajectories using a provided lagtime that
@@ -19,5 +20,19 @@ def estimateMSM(trajectories,lagtime):
 def calculatePCCA(MSM_object, numPCCA):
     """ Coarse-cluster the MSM usin numPCCA clusters. 
     Return a PCCA object"""
-    MSM_object.pccs(numPCCA)
+    MSM_object.pcca(numPCCA)
     return MSM_object
+
+def is_connected(MSM_object):
+    """ Check if the MSM created is connected"""
+    return msm.analysis.is_connected(MSM_object.transition_matrix)
+
+def ChapmanKolmogorovTest(MSM_object, nsets,memberships=None, error_estimation=False):
+    """ Perform the ChapmanKolmogorov test to validate the MSM"""
+    return MSM_object.cktest(nsets,memberships=memberships,err_est=error_estimation)
+
+def plotChapmanKolmogorovTest(CKObject, layout=None, padding_between=0.1,
+                              padding_top=0.075):
+    """ Plot the results of the Chapman-Kolgomorov tests"""
+    mplt.plot_cktest(CKObject,layout=layout, padding_between=padding_between,
+                     padding_top=padding_top)
