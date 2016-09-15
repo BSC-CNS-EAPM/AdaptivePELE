962c15f787c1105104ca03b88009f393bc1d4ac0
D.Lecina <daniel@Daniels-MacBook-Pro.local>
2016-09-16 00:09:12 +0200

    Now, thresholdCalculators can be changed from within control file

--- clustering/clusteringTypes.py
+++ clustering/clusteringTypes.py
@@ -7,3 +7,11 @@ CLUSTERING_TYPE_TO_STRING_DICTIONARY = {
     CLUSTERING_TYPES.contacts : blockNames.ClusteringTypes.contacts,
     CLUSTERING_TYPES.contactMap : blockNames.ClusteringTypes.contactMap
 }
+
+class THRESHOLD_CALCULATOR_TYPES:
+    heaviside, constant = range(2)
+
+THRESHOLD_CALCULATOR_TYPE_TO_STRING_DICTIONARY = {
+    THRESHOLD_CALCULATOR_TYPES.heaviside : blockNames.ThresholdCalculator.heaviside,
+    THRESHOLD_CALCULATOR_TYPES.constant : blockNames.ThresholdCalculator.constant
+}
