0f303693bedc2b402611c5cd3561c9c99da74c4a
Daniel Lecina <daniel@Daniels-MacBook-Pro.local>
2016-09-15 19:28:53 +0200

    Add missing comma

--- clustering/clusteringTypes.py
+++ clustering/clusteringTypes.py
@@ -4,6 +4,6 @@ class CLUSTERING_TYPES:
     contacts, contactMap = range(2)
 
 CLUSTERING_TYPE_TO_STRING_DICTIONARY = {
-    CLUSTERING_TYPES.contacts : blockNames.ClusteringTypes.contacts
+    CLUSTERING_TYPES.contacts : blockNames.ClusteringTypes.contacts,
     CLUSTERING_TYPES.contactMap : blockNames.ClusteringTypes.contactMap
 }
