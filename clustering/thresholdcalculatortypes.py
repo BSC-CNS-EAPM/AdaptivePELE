0cc7d25e5ca948627bda8680278bf79483321f89
D.Lecina <daniel.lecina@bsc.es>
2016-09-16 11:22:21 +0200

    Moved threshold calculator function to a diferent file

---
+++ clustering/thresholdcalculatortypes.py
@@ -0,0 +1,9 @@
+import blockNames
+
+class THRESHOLD_CALCULATOR_TYPES:
+    heaviside, constant = range(2)
+
+THRESHOLD_CALCULATOR_TYPE_TO_STRING_DICTIONARY = {
+    THRESHOLD_CALCULATOR_TYPES.heaviside : blockNames.ThresholdCalculator.heaviside,
+    THRESHOLD_CALCULATOR_TYPES.constant : blockNames.ThresholdCalculator.constant
+}
