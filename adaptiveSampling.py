97b368df08c6167ffe3ded9304d0e44260001ce9
(D. Lecina) <(danilecina@gmail.com)>
2016-09-16 13:19:09 +0200

    Removed all code related to pyproct clustering

--- adaptiveSampling.py
+++ adaptiveSampling.py
@@ -33,20 +33,6 @@ def createMultipleComplexesFilenames(numberOfSnapshots, inputFileTemplate, tmpIn
     jsonString += inputFileTemplate%(tmpInitialStructuresTemplate%(iteration,numberOfSnapshots-1))
     return jsonString
 
-def generateFilteredListSnapshotSelectionString(currentEpoch, compressionPath):
-    selectionString = ""
-    for i in range(currentEpoch):
-        selectionString += " \"" + compressionPath%i + "\", "
-    selectionString += " \"" + compressionPath%currentEpoch + "\""
-    return selectionString
-
-def generateSnapshotSelectionString(currentEpoch, outputPathTempletized, trajectoryWildcard):
-    selectionString = ""
-    for i in range(currentEpoch):
-        selectionString += " \"" + os.path.join(outputPathTempletized%i, trajectoryWildcard) + "\", "
-    selectionString += " \"" + os.path.join(outputPathTempletized%currentEpoch, trajectoryWildcard) + "\""
-    return selectionString
-
 def generateSnapshotSelectionStringLastRound(currentEpoch, outputPathTempletized, trajectoryWildcard):
     return " \"" + os.path.join(outputPathTempletized%currentEpoch, trajectoryWildcard) + "\""
 
@@ -54,120 +40,7 @@ def countEpochAcceptedSteps(folder):
     acceptedSteps = os.popen("tail -qn 1 " + os.path.join(folder,"*report_*") + " | awk '{a+=$3}END{print a}'").read()
     return acceptedSteps
 
-def runPyProCT(pyProctControlFile, PYTHON, PYPROCT):
-    # example to run pyproct in life: /data2/apps/PYTHON/2.7.5/bin/python2.7 -m pyproct.main controlFile
-    toRun = [PYTHON, "-m", PYPROCT, pyProctControlFile]
-    toRun = " ".join(toRun)
-    print toRun
-    startTime = time.time()
-    proc = subprocess.Popen(toRun, stdout=subprocess.PIPE, shell=True)
-    (out, err) = proc.communicate()
-    print out
-    if err: print err
-
-    endTime = time.time() 
-    print "Pyproct took %.2f sec" % (endTime - startTime)
-
-def getFileAndSnapshot(model):
-    splitModel = model.split("\n")
-    origFile = ""
-    origSnapshot = 0
-    for line in splitModel:
-        if "REMARK source" in line:
-            origFile = line.split(":")[1].strip()
-        if "REMARK original model nr" in line:
-            origSnapshot = int(line.split(":")[1]) - 1
-            break
-    
-    return origFile, origSnapshot
-
-
-def getSnapshot(file, snapshot):
-    inputFile = open(file, "r")
-    inputFileContent = inputFile.read()
-    return inputFileContent.split("ENDMDL")[snapshot]
-
-def calculateEachClustersPopulation(resultsJSON):
-    jsonFile = open(resultsJSON, 'r').read()
-    parsedJSON = json.loads(jsonFile)
-
-    bestClustering = parsedJSON["best_clustering"]
-    clusters = parsedJSON["selected"][bestClustering]["clustering"]
-    clustersObject = Clustering.from_dic(clusters)
-    sizes = np.array([])
-    for cluster in clustersObject.clusters:
-        sizes = np.append(sizes, cluster.get_size())
-    return sizes
-
-def calculateDegeneracy(sizes, spawningType, numberOfProcessors):
-    if spawningType == "SAME_WEIGHT":
-        return [1] * len(sizes)
-    else:
-        if isinstance(sizes, list): sizes = np.array(sizes)
-
-        weights = 1./sizes
-        weights /= sum(weights)
-
-        degeneracy = []
-        if spawningType == "INVERSELY_PROPORTIONAL":
-            trajToDistribute = numberOfProcessors-1
-            for i, weight in enumerate(weights):
-                degeneracy.append(int(weight*trajToDistribute)) 
-
-        elif spawningType == "AT_LEAST_ONE":
-            numberOfInitialStructures = len(weights)
-            trajToDistribute = numberOfProcessors-1-numberOfInitialStructures
-            for i, weight in enumerate(weights):
-                degeneracy.append(1 + int(weight*trajToDistribute)) #at least one per cluster
-
-
-        decimalPart = []
-        for i, weight in enumerate(weights):
-            decimalPart.append(math.modf(weight*trajToDistribute)[0])
-        sortedDecimals = np.argsort(decimalPart)
-        sortedDecimals = sortedDecimals[::-1]
-
-        leftProcessors = numberOfProcessors-1-sum(degeneracy)
-        for i in range(leftProcessors):
-            degeneracy[sortedDecimals[i]] += 1
-
-        return degeneracy
-
-def calculateDegeneracyOfClusterRepresentatives(representativesFile, resultsJSON, spawningType, numberOfProcessors):
-    sizes = calculateEachClustersPopulation(resultsJSON)
-    return calculateDegeneracy(sizes, spawningType, numberOfProcessors) 
-
-def replaceLigandFileForFullProteinFile(origFile, ligandfileBasename, trajectoryBasename):
-    newFile = origFile.replace(ligandfileBasename, trajectoryBasename)
-    foundFiles = glob.glob(newFile)
-    if len(foundFiles) > 1:
-        sys.exit("Found " + str(foundFiles) + " that match pattern for full trajectory.")
-    if len(foundFiles) == 0:
-        sys.exit("Found no full trajectories that match pattern: \"" + trajectoryBasename + "\"")
-    return foundFiles[0]
-
-def makeClusterRepresentativesInitialStructures(representativesFile, tmpInitialStructuresTemplate, degeneracyOfRepresentatives, ligandfileBasename, trajectoryBasename):
-    models = atomset.getPDBSnapshots(representativesFile)
-
-    counts = 0
-    for i, model in enumerate(models):
-        origFile, origSnapshot = getFileAndSnapshot(model)
-        replacedFile = replaceLigandFileForFullProteinFile(origFile, ligandfileBasename, trajectoryBasename) 
-        origModel = getSnapshot(replacedFile, origSnapshot)
-
-        for j in range(int(degeneracyOfRepresentatives[i])):
-            outputFilename = tmpInitialStructuresTemplate%counts
-            print 'Writing to ', outputFilename, replacedFile, origSnapshot
-            representativeFile = open(outputFilename, 'w')
-            representativeFile.write(origModel)
-            representativeFile.close()
-
-            counts += 1
-
-    print "counts & cluster centers", counts, len(models)
-    return counts
-
-def makeOwnClusteringClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clustering, iteration):
+def makeClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clustering, iteration):
 
     counts = 0
     for i, cluster in enumerate(clustering.clusters.clusters):
@@ -209,69 +82,6 @@ def saveInitialControlFile(jsonParams, originalControlFile):
     file = open(originalControlFile, 'w')
     jsonFile = open(jsonParams, 'r').read()
     file.write(jsonFile)
-
-
-
-def grepTraj(epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory, runNumber):
-    fullTrajFilename = os.path.join(outputPathTempletized%epoch, trajectoryWildcard%runNumber)
-    ligandTrajFilename = os.path.join(outputPathTempletized%epoch, ligandTrajectory%str(runNumber))
-    toRun = "grep -E \"^HETATM...........%s|ATOM.........CA|MODEL|ENDMDL\" "%ligandResname + fullTrajFilename + " | grep -v \"\-\-\|REMARK\" > " + ligandTrajFilename
-    #toRun = "grep -E \"^HETATM...........%s|MODEL|ENDMDL\" "%ligandResname + fullTrajFilename + " | grep -v \"\-\-\|REMARK\" > " + ligandTrajFilename
-    #toRun = "cp " + fullTrajFilename + " " + ligandTrajFilename
-    proc = subprocess.Popen(toRun, shell=True)
-    proc.communicate()
-
-def grepLigand(processors, epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory):
-
-    func = partial(grepTraj, epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory)
-
-    iterable = range(1,processors)
-
-    pool = multiprocessing.Pool(processors)
-    pool.map(func, iterable)
-    pool.close()
-    pool.join()
-
-    #for runNumber in range(1, processors):
-    #    grepTraj(runNumber, epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory)
-
-""" ORIG SERIAL
-def grepLigand(processors, epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory):
-    for runNumber in range(1, processors):
-        fullTrajFilename = os.path.join(outputPathTempletized%epoch, trajectoryWildcard%runNumber)
-        ligandTrajFilename = os.path.join(outputPathTempletized%epoch, ligandTrajectory%str(runNumber))
-        toRun = "grep -E \"^HETATM...........%s|ATOM.........CA|MODEL|ENDMDL\" "%ligandResname + fullTrajFilename + " | grep -v \"\-\-\|REMARK\" > " + ligandTrajFilename
-        #toRun = "grep -E \"^HETATM...........%s|MODEL|ENDMDL\" "%ligandResname + fullTrajFilename + " | grep -v \"\-\-\|REMARK\" > " + ligandTrajFilename
-        #toRun = "cp " + fullTrajFilename + " " + ligandTrajFilename
-        proc = subprocess.Popen(toRun, shell=True)
-        proc.communicate()
-"""
-
-"""
- #MAYBE GOOD TO HAVE FOR A NEAR FUTURE
- def read_in_chunks(size_in_bytes, processors, epoch, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory):
-    s = 'Lets say i have a text file of 1000 GB'
-    with open('data.txt', 'r+b') as f:
-        prev = ''
-        count = 0
-        f_read  = partial(f.read, size_in_bytes)
-        for text in iter(f_read, ''):
-            if not text.endswith('\n'):
-                # if file contains a partial line at the end, then don't
-                # use it when counting the substring count. 
-                text, rest = text.rsplit('\n', 1)
-                # pre-pend the previous partial line if any.
-                text =  prev + text
-                prev = rest
-            else:
-                # if the text ends with a '\n' then simple pre-pend the
-                # previous partial line. 
-                text =  prev + text
-                prev = ''
-            count += text.count(s)
-        count += prev.count(s)
-        print count
-"""
 
 def main(jsonParams=None):
     if jsonParams is None:
@@ -362,42 +172,18 @@ def main(jsonParams=None):
         epsilon_file.write("Iteration\tEpsilon\n")
         epsilon_file.close()
 
-
-
-
     if restart:
         firstRun = findFirstRun(outputPath, CLUSTERING_OUTPUT_OBJECT)
 
         if firstRun != 0:
-            """
-            degeneracyOfRepresentatives = calculateDegeneracyOfClusterRepresentatives(PYPROCT_REPRESENTATIVE_OUTPUT%(firstRun-1), PYPROCT_RESULTS_OUTPUT%(firstRun-1), spawning, processors)
-            seedingPoints = makeClusterRepresentativesInitialStructures(PYPROCT_REPRESENTATIVE_OUTPUT%(firstRun-1), tmpInitialStructuresTemplate, degeneracyOfRepresentatives, ligandTrajectoryBasename, trajectoryBasename)
-            """
-
-
-
-            #TO BE REMOVED
-            """
-            snapshotsJSONSelectionString = generateSnapshotSelectionString(firstRun-1, outputPathTempletized, trajectoryBasename)
-            snapshotsJSONSelectionString = "[" + snapshotsJSONSelectionString + "]"
-            paths = eval(snapshotsJSONSelectionString)
-            clusteringMethod = clustering.Clustering(ligandResname)
-            clusteringMethod.cluster(paths, withinClusterThreshold)
-            """
-
             with open(CLUSTERING_OUTPUT_OBJECT%(firstRun), 'rb') as f:
                 clusteringMethod = pickle.load(f)
 
-
-            sizes = []
-            for cluster in clusteringMethod.clusters.clusters:
-                sizes.append(cluster.elements)
-            #degeneracyOfRepresentatives = calculateDegeneracy(sizes, spawning, processors)
             degeneracyOfRepresentatives = startingConformationsCalculator.calculate(clusteringMethod.clusters.clusters, simulationRunner.parameters.processors-1, spawningParams, firstRun)
             startingConformationsCalculator.log()
             print "Degeneracy", degeneracyOfRepresentatives
 
-            seedingPoints = makeOwnClusteringClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clusteringMethod, firstRun)
+            seedingPoints = makeClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clusteringMethod, firstRun)
 
             initialStructuresAsString = createMultipleComplexesFilenames(seedingPoints, inputFileTemplate, tmpInitialStructuresTemplate, firstRun)
             
@@ -412,12 +198,6 @@ def main(jsonParams=None):
 
 
     peleControlFileDictionary = {"COMPLEXES":initialStructuresAsString, "PELE_STEPS":simulationRunner.parameters.peleSteps}
-    """
-    pyproctControlFileDictionary = {"MIN_SEEDING_POINTS":minSeedingPoints, "MAX_SEEDING_POINTS":maxSeedingPoints, "NUMBER_OF_PROCESSORS":processors, "RESNAME": ligandResname}
-
-    if debug:
-        pyproctControlFileDictionary["NUMBER_OF_PROCESSORS"] = 8
-    """
 
     #Make control file
     outputDir = outputPathTempletized%firstRun
@@ -441,51 +221,6 @@ def main(jsonParams=None):
             endTime = time.time() 
             print "PELE %s sec" % (endTime - startTime)
 
-            #filter ligand
-            """
-            startTime = time.time() 
-            grepLigand(processors, i, ligandResname, outputPathTempletized, trajectoryWildcard, ligandTrajectory)
-            endTime = time.time() 
-            print "Grep ligand: %s sec" % (endTime - startTime)
-            """
-
-        #allsnapshotsjsonselectionstring = generatesnapshotselectionstring(i)
-        """
-        lastRoundSnapshotsJSONSelectionString = generateSnapshotSelectionStringLastRound(i, outputPathTempletized, ligandTrajectory%"*")
-        pyproctControlFileDictionary["COMPLEXES"] = lastRoundSnapshotsJSONSelectionString
-        pyproctControlFileDictionary["OUTPUT_PATH"] = PYPROCT_OUTPUT_DIR_SINGLE_EPOCH%i
-        typicalAcceptance = 0.3
-        compressionFactor = 0.1
-        estimatedAcceptedSteps = (processors-1)*peleSteps*typicalAcceptance*compressionFactor
-        estimatedAcceptedSteps = max(1,estimatedAcceptedSteps) #at least one
-        pyproctControlFileDictionary["COMPRESSION"] = PYPROCT_COMPRESSION_BLOCK%min(estimatedAcceptedSteps, countEpochAcceptedSteps(outputPathTempletized%i))
-        pyproctControlFileDictionary["CLUSTERING"] = "gromos"
-        #pyproctControlFileDictionary["MAX_SEEDING_POINTS"] = 32
-        makeWorkingControlFile(TEMPLETIZED_PYPROCT_CONTROLFILENAME, tmpControlFilename_pyproct_thisEpoch%i, pyproctControlFileDictionary) 
-
-
-        print "Filtering current epoch..."
-        startTime = time.time() 
-        runPyProCT(tmpControlFilename_pyproct_thisEpoch%i, PYTHON, PYPROCT) #Handle case when pyProCt fails
-        endTime = time.time() 
-        print "Pyproct filtering ligand: %s sec" % (endTime - startTime)
-
-
-        print "Clustering with filtered snapshots"
-        filteredList = generateFilteredListSnapshotSelectionString(i, PYPROCT_COMPRESSION_OUTPUT)
-        pyproctControlFileDictionary["COMPLEXES"] = filteredList
-        pyproctControlFileDictionary["OUTPUT_PATH"] = PYPROCT_OUTPUT_DIR%i
-        pyproctControlFileDictionary["COMPRESSION"] = "" #won't be use, we'll use the single trajectory compressed/filtered list
-        pyproctControlFileDictionary["CLUSTERING"] = "gromos"
-        #pyproctControlFileDictionary["MAX_SEEDING_POINTS"] = maxSeedingPoints
-        makeWorkingControlFile(TEMPLETIZED_PYPROCT_CONTROLFILENAME, tmpControlFilename_pyproct%i, pyproctControlFileDictionary) 
-
-        print "Clustering..."
-        startTime = time.time() 
-        runPyProCT(tmpControlFilename_pyproct%i, PYTHON, PYPROCT) #Handle case when pyProCt fails
-        endTime = time.time() 
-        print "Pyproct clustering ligand: %s sec" % (endTime - startTime)
-        """
 
         print "Clustering..."
         startTime = time.time() 
@@ -508,15 +243,11 @@ def main(jsonParams=None):
         print "Clustering ligand: %s sec" % (endTime - startTime)
 
 
-        """
-        degeneracyOfRepresentatives = calculateDegeneracyOfClusterRepresentatives(PYPROCT_REPRESENTATIVE_OUTPUT%i, PYPROCT_RESULTS_OUTPUT%i, spawning, processors)
-        numberOfSeedingPoints = makeClusterRepresentativesInitialStructures(PYPROCT_REPRESENTATIVE_OUTPUT%i, tmpInitialStructuresTemplate, degeneracyOfRepresentatives, ligandTrajectoryBasename, trajectoryBasename)
-        """
         degeneracyOfRepresentatives = startingConformationsCalculator.calculate(clusteringMethod.clusters.clusters, simulationRunner.parameters.processors-1, spawningParams, i)
         startingConformationsCalculator.log()
         print "Degeneracy", degeneracyOfRepresentatives
 
-        numberOfSeedingPoints = makeOwnClusteringClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clusteringMethod, i+1)
+        numberOfSeedingPoints = makeClusterRepresentativesInitialStructures(tmpInitialStructuresTemplate, degeneracyOfRepresentatives, clusteringMethod, i+1)
 
         clusteringMethod.writeOutput(CLUSTERING_OUTPUT_DIR%i, degeneracyOfRepresentatives, CLUSTERING_OUTPUT_OBJECT%i)
         if i > 0:
