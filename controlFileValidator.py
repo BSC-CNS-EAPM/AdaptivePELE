218d8266c914045fda965187f3f4ac5082091c31
(D. Lecina) <(danilecina@gmail.com)>
2016-09-23 10:43:01 +0200

    controlFileValidator now warns of all mistakes of control file before throwing an error

--- controlFileValidator.py
+++ controlFileValidator.py
@@ -1,79 +1,116 @@
 import validatorBlockNames
 import blockNames
 import json
+import warnings
+
 
 def validate(control_file):
-    
-    with open(control_file,'r') as f:
+    isCorrect = True
+    with open(control_file, 'r') as f:
         jsonFile = f.read()
     parsedJSON = json.loads(jsonFile)
     for block in dir(validatorBlockNames.ControlFileParams):
         if block.startswith('__'):
             continue
         block_obj = getattr(validatorBlockNames,
-                            eval("validatorBlockNames.ControlFileParams.%s"%block))
-        controlfile_obj = eval("blockNames.ControlFileParams.%s"%block)
-        #try:
+                            eval("validatorBlockNames.ControlFileParams.%s" % block))
+        controlfile_obj = eval("blockNames.ControlFileParams.%s" % block)
+
         if block == "generalParams":
-            validateGeneralBlock(block_obj, parsedJSON[controlfile_obj])
+            try:
+                blockCorrect = validateGeneralBlock(block_obj, parsedJSON[controlfile_obj])
+                isCorrect = isCorrect and blockCorrect
+            except KeyError:
+                warnings.warn("Block %s not found in control file!" % controlfile_obj)
+                isCorrect = False
         else:
-            validateBlock(block_obj, parsedJSON[controlfile_obj])
-        #except KeyError:
-        #    raise KeyError("Block %s not found in control file!"%controlfile_obj)
-    print "Congratulations! No errors found!"
+            try:
+                blockCorrect = validateBlock(block_obj, parsedJSON[controlfile_obj])
+                isCorrect = isCorrect and blockCorrect
+            except KeyError:
+                warnings.warn("Block %s not found in control file!" % controlfile_obj)
+                isCorrect = False
+    if isCorrect:
+        print "Congratulations! No errors found!"
+    else:
+        raise ValueError("There are errors in your control file")
     return True
 
+
 def validateBlock(blockName, controlFileBlock):
+    isCorrect = True
     blockType = controlFileBlock["type"]
-    #Check if type selected is valid
+    # Check if type selected is valid
     try:
-        assert isinstance(blockType,unicode),"Type for %s should be %s and instead is %s"%(blockType, 'unicode', type(blockType).__name__)
+        if not isinstance(blockType, unicode):
+            warnings.warn("Type for %s should be %s and instead is %s" % (blockType, 'unicode', type(blockType).__name__))
+            isCorrect = False
     except KeyError:
-        raise KeyError("Type %s in %s not found."%(blockType,blockName.__name__))
+        warnings.warn("Type %s in %s not found." % (blockType, blockName.__name__))
+        isCorrect = False
     # check for mandatory parameters
-    for mandatory,value in blockName.types[blockType].iteritems():
+    for mandatory, value in blockName.types[blockType].iteritems():
         try:
-            assert isinstance(controlFileBlock['params'][mandatory],eval(value)),"Type for %s should be %s and instead is %s"%(mandatory, value, type(controlFileBlock['params'][mandatory]).__name__)
+            if not isinstance(controlFileBlock['params'][mandatory], eval(value)):
+                warnings.warn("Type for %s should be %s and instead is %s" % (mandatory, value, type(controlFileBlock['params'][mandatory]).__name__))
+                isCorrect = False
         except KeyError as err:
-            raise KeyError("%s missing: Mandatory parameter %s in %s not found."%(err.message,mandatory,blockName.__name__)) 
+            warnings.warn("%s missing: Mandatory parameter %s in %s not found." % (err.message, mandatory, blockName.__name__))
+            isCorrect = False
     # check rest of parameters specified
     for param, value in controlFileBlock["params"].iteritems():
         try:
-            assert isinstance(value,eval(blockName.params[param])),"Type for %s should be %s and instead is %s"%(param, blockName.params[param], type(value).__name__)
+            if not isinstance(value, eval(blockName.params[param])):
+                warnings.warn("Type for %s should be %s and instead is %s" % (param, blockName.params[param], type(value).__name__))
+                isCorrect = False
         except KeyError:
-            raise KeyError("Parameter %s not recognized."%param) 
+            warnings.warn("Parameter %s not recognized." % param)
+            isCorrect = False
 
     for block in dir(blockName):
         if not block.startswith('__') and block != "params" and block != "types":
-            types_dict = eval("blockName.%s"%block)["types"]
-            params_dict = eval("blockName.%s"%block)["params"]
+            types_dict = eval("blockName.%s" % block)["types"]
+            params_dict = eval("blockName.%s" % block)["params"]
             try:
                 blockType = controlFileBlock[block]["type"]
                 try:
-                    assert isinstance(blockType,str),"Type for %s should be %s and instead is %s"%(blockType, 'str', type(blockType).__name__)
+                    if not isinstance(blockType, str):
+                        warnings.warn("Type for %s should be %s and instead is %s" % (blockType, 'str', type(blockType).__name__))
+                        isCorrect = False
                     typecheck = types_dict[blockType]
                 except KeyError:
-                    raise KeyError("Type %s in %s not found."%(blockType,blockName.__name__))
+                    warnings.warn("Type %s in %s not found." % (blockType, blockName.__name__))
+                    isCorrect = False
             # check rest of parameters specified
                 for param, value in controlFileBlock[block].iteritems():
                     try:
-                        assert isinstance(value,eval(params_dict[param])),"Type for %s should be %s and instead is %s"%(param, params_dict[param], type(value).__name__)
+                        if not isinstance(value, eval(params_dict[param])):
+                            warnings.warn("Type for %s should be %s and instead is %s" % (param, params_dict[param], type(value).__name__))
+                            isCorrect = False
                     except KeyError:
-                        raise KeyError("Parameter %s not recognized."%param) 
+                        warnings.warn("Parameter %s not recognized." % param)
+                        isCorrect = False
             except KeyError:
-                #The parameters blocks for density and threshold calculator are
-                #not mandatory
+                # The parameters blocks for density and threshold calculator are
+                # not mandatory
                 pass
+    return isCorrect
+
 
 def validateGeneralBlock(blockName, controlFileBlock):
-    for key,value in controlFileBlock.iteritems():
+    isCorrect = True
+    for key, value in controlFileBlock.iteritems():
         try:
-            assert isinstance(controlFileBlock[key],eval(blockName.params[key])),"Type for %s should be %s and instead is %s"%(key, value, type(controlFileBlock[key]).__name__)
+            if not isinstance(controlFileBlock[key], eval(blockName.params[key])):
+                warnings.warn("Type for %s should be %s and instead is %s" % (key, value, type(controlFileBlock[key]).__name__))
+                isCorrect = False
         except KeyError:
-            raise KeyError("Mandatory parameter %s in GeneralParams not found."%key) 
+            warnings.warn("Mandatory parameter %s in GeneralParams not found." % key)
+            isCorrect = False
+    return isCorrect
 
 if __name__ == "__main__":
-    controlFiles = ["tests/data/3ptb_data/integrationTest%i.conf"%i for i in range(1,4)]
+    controlFiles = ["tests/data/3ptb_data/integrationTest%i.conf" % i for i in range(1, 4)]
     for contfile in controlFiles:
-        print "Validating control file %s"%contfile
+        print "Validating control file %s" % contfile
         validate(contfile)
