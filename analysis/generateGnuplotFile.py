361e1f0a08e05f1c7a5ef79dab30a505082182c2
jgilaber <joan.gilabert@bsc.es>
2016-09-26 11:53:33 +0200

    Added boltzmann weigths for random selection of pdb in contactMap clustering + small style changes

--- analysis/generateGnuplotFile.py
+++ analysis/generateGnuplotFile.py
@@ -4,15 +4,15 @@ import glob
 import types
 
 
-
 def parseArguments():
     desc = "Generates output for gnuplot\n"\
             "It MUST be run from the root epoch folder (i.e., where it can find the folders 0/, 1/, 2/, ... lastEpoch/"\
             "To be run for example like: \">python generateGnuplotFile.py | gnuplot -persist\""
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("steps", type=int, default=25, help="Pele steps per run") 
+    parser.add_argument("steps", type=int, default=25, help="Pele steps per run")
     args = parser.parse_args()
     return args.steps
+
 
 def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps=False, replotFirst=False):
     """
@@ -24,7 +24,7 @@ def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRu
 
 
     dictionary = {'reportName':reportName, 'col2':column2, 'numberOfEpochs':numberOfEpochs, 'withLines':''}
-    
+
     #runs of epoch 0, assumed constant
     numberOfRunsPerEpoch=len( glob.glob( os.path.join(str(0), reportName+"*") ) )
     dictionary['runsPerEpoch'] = numberOfRunsPerEpoch
@@ -81,74 +81,68 @@ def generateForLoopString(gnuplotString, reportName, column1, column2, stepsPerR
     print plottingString
 
 
-
 def main():
     stepsPerRun = parseArguments()
 
-
-    #VARIABLES TO SET WHEN PRINTING
+    # VARIABLES TO SET WHEN PRINTING
     kindOfPrint = "PRINT_RMSD_STEPS"
-    #kindOfPrint = "PRINT_BE_RMSD"
+    # kindOfPrint = "PRINT_BE_RMSD"
 
     ownClustering = True
 
     if kindOfPrint == "PRINT_RMSD_STEPS":
         printWithLines = True
-        column1 = 2 #steps
-        #column2 = 7 #rmsd
+        column1 = 2  # steps
+        # column2 = 7 # rmsd
 
-        column2 = 5 #rmsd
+        column2 = 6  # rmsd
 
-        #PR
-        #column2 = 7 #sasa
+        # PR
+        # column2 = 7  # sasa
 
-        totalNumberOfSteps=True
+        totalNumberOfSteps = True
     elif kindOfPrint == "PRINT_BE_RMSD":
         printWithLines = False
-        column1 = 7 #rmsd
-        column2 = 6 #binding energy
+        column1 = 7  # rmsd
+        column2 = 6  # binding energy
 
-        column1 = 5 #rmsd
-        column2 = 6 #binding energy
+        column1 = 5  # rmsd
+        column2 = 6  # binding energy
 
-        column1 = 8 #rmsd
-        column2 = 7 #binding energy
+        column1 = 8  # rmsd
+        column2 = 7  # binding energy
 
-        #PR
-        #column1 = 7 #sasa
-        #column2 = 5 #binding energy
-        totalNumberOfSteps=False
+        # PR
+        # column1 = 7  #sasa
+        # column2 = 5  #binding energy
+        totalNumberOfSteps = False
 
     if ownClustering:
-        representativeReportName='clustering/reports'
+        representativeReportName = 'clustering/reports'
     else:
-        representativeReportName='pyproct/reports'
+        representativeReportName = 'pyproct/reports'
 
-
-
-    #reportName='run_report_'
-    reportName='report_'
+    # reportName = 'run_report_'
+    reportName = 'report_'
     """
     gnuplotString = "plot for [i=1:%(runsPerEpoch)d] \"%(epoch)d/%(reportName)s\".i u %(col1)s:%(col2)d lt 6 lc palette frac %(epoch)d/%(numberOfEpochs)d. notitle %(withLines)s"
     generateForLoopString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps, False)
-
 
     """
     gnuplotString = "plot for [j=0:%(numberOfEpochs)d-1] for [i=1:%(runsPerEpoch)d] \"\".j.\"/%(reportName)s\".i u %(col1)s:%(col2)d lt 6 lc palette frac j/%(numberOfEpochs)d. notitle %(withLines)s"
     generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps, False)
 
-
     printWithLines = False
-    
+
     if kindOfPrint == "PRINT_RMSD_STEPS":
         column1 = lambda x: (x+1)*stepsPerRun
 
-    #Comment if you don't want to plot representative info
-    #refactor
+    # Comment if you don't want to plot representative info
+    # refactor
     gnuplotString = "plot for [j=0:%(numberOfEpochs)d-1] \"\".j.\"/%(reportName)s\" u %(col1)s:%(col2)d lt 6 lc palette frac j/%(numberOfEpochs)d. notitle %(withLines)s"
-    #generateForLoopString(gnuplotString, representativeReportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps, replotFirst=True)
+    # generateForLoopString(gnuplotString, representativeReportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps, replotFirst=True)
 
-    #os.system(plottingString + "\| gnuplot -persist")
+    # os.system(plottingString + "\| gnuplot -persist")
 
 if __name__ == "__main__":
     main()
