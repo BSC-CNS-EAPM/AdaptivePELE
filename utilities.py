6d9e6497fbb957b8310232cd89cb8712559bb82a
(D. Lecina) <(danilecina@gmail.com)>
2016-10-05 11:40:24 +0200

    Added function to esxtract clusters from clustering pickle file

--- utilities.py
+++ utilities.py
@@ -1,5 +1,7 @@
 import os
-import shutil 
+import shutil
+import pickle
+from clustering import clustering
 
 def cleanup(tmpFolder):
     if os.path.exists(tmpFolder):
@@ -8,3 +10,14 @@ def cleanup(tmpFolder):
 def makeFolder(outputDir):
     if not os.path.exists(outputDir):
         os.makedirs(outputDir)
+
+def writeStructures(clusteringObject, listStructures, outputPath="cluster.pdb"):
+    with open(clusteringObject, "rb") as f:
+        clObject = pickle.load(f)
+    nameStructure = os.path.splitext(outputPath)
+    outputName = nameStructure[0]+'_%d'+nameStructure[1]
+    path = os.path.split(outputName)
+    if path[0]:
+        makeFolder(path[0])
+    for element in listStructures:
+        clObject.clusters.clusters[element].writePDB(path[1] % element)
