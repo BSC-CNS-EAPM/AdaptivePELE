319a17e45948af1f3e6ad42c32a1bf374a2ef606
(D. Lecina) <(danilecina@gmail.com)>
2016-10-07 16:51:26 +0200

    Added tests for all types of clustering

--- tests/testClustering.py
+++ tests/testClustering.py
@@ -1,21 +1,23 @@
 from clustering import clustering
 import unittest
 
+
 class clusteringTest(unittest.TestCase):
     def testCluster(self):
-        #preparation
+        # preparation
         clusteringBuilder = clustering.ClusteringBuilder()
-        clusteringDictionary = {"type" : "contacts", 
-                                "params" : {"ligandResname": "AIN","contactThresholdDistance":
-                                                             8}}
-        clusteringInstance = clusteringBuilder.buildClustering(clusteringDictionary,"ain_report", 3)
+        clusteringParams = {"type": "contacts",
+                            "params": {"ligandResname": "AIN",
+                                       "contactThresholdDistance": 8}}
+        clusteringInstance = clusteringBuilder.buildClustering(clusteringParams,
+                                                               "ain_report", 3)
 
         trajNames = ["tests/data/aspirin_data/traj*"]
 
-        #function to test
+        # function to test
         clusteringInstance.cluster(trajNames)
 
-        #assertion
+        # assertion
         allClusters = clusteringInstance.clusters.clusters
         goldenNumberOfClusters = 2
         goldenEnergyCluster1 = -8424.8
@@ -29,29 +31,108 @@ class clusteringTest(unittest.TestCase):
         self.assertEqual(allClusters[0].elements, goldenElementsCluster1)
         self.assertEqual(allClusters[1].elements, goldenElementsCluster2)
 
-    def _test_cluster_sklearn(self):
-         #preparation
-         clusteringBuilder = clustering.ClusteringBuilder()
-         clusteringInstance = clusteringBuilder.buildClustering("contactMapAffinity","AIN", "ain_report", 3)
+    def test_cluster_sklearn_affinity(self):
+        # preparation
+        clusteringParams = {"type": "contactMapAffinity",
+                            "params": {"ligandResname": "AIN",
+                                       "contactThresholdDistance": 8}}
+        clusteringBuilder = clustering.ClusteringBuilder()
+        clusteringInstance = clusteringBuilder.buildClustering(clusteringParams,
+                                                               "ain_report", 3)
 
-         trajNames = ["tests/data/aspirin_data/traj*"]
+        trajNames = ["tests/data/aspirin_data/traj*"]
 
-         #function to test
-         clusteringInstance.cluster(trajNames)
+        # function to test
+        clusteringInstance.cluster(trajNames)
 
-         #assertion
-         # Testing is diffrent now, previous assertions won't work
-         # TODO: create new assertion for the new algorithm
-         allClusters = clusteringInstance.clusters.clusters
-         clusteringInstance.clusters.printClusters()
-         goldenNumberOfClusters = 2
-         goldenEnergyCluster1 = -8424.8
-         goldenEnergyCluster2 = -8453.29
-         goldenElementsCluster1 = 2
-         goldenElementsCluster2 = 1
+        # assertion
+        # Testing is diffrent now, previous assertions won't work
+        # TODO: create new assertion for the new algorithm
+        allClusters = clusteringInstance.clusters.clusters
+        clusteringInstance.clusters.printClusters()
+        goldenNumberOfClusters = 2
+        goldenEnergyCluster1 = -8424.8
+        goldenEnergyCluster2 = -8453.29
+        goldenElementsCluster1 = 2
+        goldenElementsCluster2 = 1
 
-         self.assertEqual(len(allClusters), goldenNumberOfClusters)
-         self.assertAlmostEqual(allClusters[0].metric, goldenEnergyCluster1, 2)
-         self.assertAlmostEqual(allClusters[1].metric, goldenEnergyCluster2, 2)
-         self.assertEqual(allClusters[0].elements, goldenElementsCluster1)
-         self.assertEqual(allClusters[1].elements, goldenElementsCluster2)
+        self.assertEqual(len(allClusters), goldenNumberOfClusters)
+        self.assertAlmostEqual(allClusters[0].metric, goldenEnergyCluster1, 2)
+        self.assertAlmostEqual(allClusters[1].metric, goldenEnergyCluster2, 2)
+        self.assertEqual(allClusters[0].elements, goldenElementsCluster1)
+        self.assertEqual(allClusters[1].elements, goldenElementsCluster2)
+
+    def test_cluster_sklearn_agglomerative(self):
+        # preparation
+        nclusters = 2
+        clusteringParams = {"type": "contactMapAgglomerative",
+                            "params": {"ligandResname": "AIN",
+                                       "contactThresholdDistance": 8,
+                                       "nclusters": nclusters}}
+        clusteringBuilder = clustering.ClusteringBuilder()
+        clusteringInstance = clusteringBuilder.buildClustering(clusteringParams,
+                                                               "ain_report", 3)
+
+        trajNames = ["tests/data/aspirin_data/traj*"]
+
+        # function to test
+        clusteringInstance.cluster(trajNames)
+
+        # assertion
+        # Testing is diffrent now, previous assertions won't work
+        # TODO: create new assertion for the new algorithm
+        allClusters = clusteringInstance.clusters.clusters
+        clusteringInstance.clusters.printClusters()
+        goldenNumberOfClusters = nclusters
+        goldenEnergyCluster1 = -8424.8
+        goldenEnergyCluster2 = -8453.29
+        goldenElementsCluster1 = 2
+        goldenElementsCluster2 = 1
+
+        self.assertEqual(len(allClusters), goldenNumberOfClusters)
+        self.assertAlmostEqual(allClusters[0].metric, goldenEnergyCluster1, 2)
+        self.assertAlmostEqual(allClusters[1].metric, goldenEnergyCluster2, 2)
+        self.assertEqual(allClusters[0].elements, goldenElementsCluster1)
+        self.assertEqual(allClusters[1].elements, goldenElementsCluster2)
+
+    def test_cluster_accumulative(self):
+        # preparation
+        clusteringParams = {"type": "contactMapAccumulative",
+                            "params": {"ligandResname": "AIN",
+                                       "contactThresholdDistance": 8,
+                                       "similarityEvaluator": "correlation"},
+                            "thresholdCalculator": {
+                                "type": "constant",
+                                "params": {
+                                    "value": 0.15
+                                }
+                            }}
+        clusteringBuilder = clustering.ClusteringBuilder()
+        clusteringInstance = clusteringBuilder.buildClustering(clusteringParams,
+                                                               "ain_report", 3)
+
+        trajNames = ["tests/data/aspirin_data/traj*"]
+
+        # function to test
+        clusteringInstance.cluster(trajNames)
+
+        # assertion
+        # Testing is diffrent now, previous assertions won't work
+        # TODO: create new assertion for the new algorithm
+        allClusters = clusteringInstance.clusters.clusters
+        clusteringInstance.clusters.printClusters()
+        goldenNumberOfClusters = 3
+        goldenEnergyCluster1 = -8421.5
+        goldenEnergyCluster2 = -8424.8
+        goldenEnergyCluster3 = -8453.29
+        goldenElementsCluster1 = 1
+        goldenElementsCluster2 = 1
+        goldenElementsCluster3 = 1
+
+        self.assertEqual(len(allClusters), goldenNumberOfClusters)
+        self.assertAlmostEqual(allClusters[0].metric, goldenEnergyCluster1, 2)
+        self.assertAlmostEqual(allClusters[1].metric, goldenEnergyCluster2, 2)
+        self.assertAlmostEqual(allClusters[2].metric, goldenEnergyCluster3, 2)
+        self.assertEqual(allClusters[0].elements, goldenElementsCluster1)
+        self.assertEqual(allClusters[1].elements, goldenElementsCluster2)
+        self.assertEqual(allClusters[2].elements, goldenElementsCluster3)
