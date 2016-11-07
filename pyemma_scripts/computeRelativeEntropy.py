2acbba27a8789fe46527e63f8cc87cd3b006f8a5
D. Lecina <danilecina@gmail.com>
2016-11-07 15:32:03 +0100

    Improved script to compute relative entropy between two MSM

--- pyemma_scripts/computeRelativeEntropy.py
+++ pyemma_scripts/computeRelativeEntropy.py
@@ -44,17 +44,20 @@ def getTransitionMatrix(trajectoryFolder, trajectoryBasename, numClusters, lagti
     return transition, MSM_object.stationary_distribution, lagtime
 
 
-def assignNewTrajecories(X, lagtime, sampleSize=None):
+def assignNewTrajecories(X, goldenMSMClusterCenters, lagtime, ntrajs=None, length=None):
 
-    if sampleSize:
-        indices = np.array(np.random.choice(range(len(X)), sampleSize))
-        Xsample = map(lambda x: X[x], indices)
+    if ntrajs:
+        indices = np.array(np.random.choice(range(len(X)), ntrajs))
     else:
-        Xsample = X
-    assign = AssignCenters("discretized/clusterCenters.dat")
+        indices = range(len(X))
+
+    Xsample = map(lambda x: X[x][:length], indices)
+
+    assign = AssignCenters(goldenMSMClusterCenters)
     dTrajs = assign.assign(Xsample)
-    secondMSM = msm.estimateMSM(dTrajs, lagtime)
-    counts = secondMSM.count_matrix_full
+    counts = markov.estimateCountMatrix(dTrajs, goldenMSMClusterCenters.shape[0], lagtime)
+    #secondMSM = msm.estimateMSM(dTrajs, lagtime)
+    #counts = secondMSM.count_matrix_full
     counts += 1.0/counts.shape[0]
     transition = markov.buildTransitionMatrix(counts)
     return transition
@@ -64,24 +67,62 @@ def main(controlFile):
     trajectoryFolder, trajectoryFolder2, trajectoryBasename, numClusters, lagtimes, itsOutput, numberOfITS, itsErrors, lagtime, sampleSize, numRuns = readParams(controlFile)
     refTransition, refStationaryDist, lagtime = getTransitionMatrix(trajectoryFolder, trajectoryBasename, numClusters, lagtimes, itsOutput, numberOfITS, itsErrors, lagtime)
     X = trajectories.loadCOMFiles(trajectoryFolder2, trajectoryBasename)
+
+    goldenMSMClusterCenters = np.loadtxt("discretized/clusterCenters.dat")
+
+    #np.random.seed(250793)
+
     entropies = []
     try:
-        allSizes = range(5, sampleSize, 5)
+        numberOfTrajs = range(5,20,5) + range(20, sampleSize, 20)
+
+        #only trying different traj lengths if sampleSize is defined in control file
+        shortestTrajSize = min([len(i) for i in X])
+        lowerLimit = 2*lagtime
+        upperLimit = shortestTrajSize
+        #allTrajLengths = range(lowerLimit, upperLimit, 200)
+        allTrajLengths = range(lowerLimit, 550, 50) + range(750, upperLimit, 250)
+        #allTrajLengths = [None]
     except TypeError:
-        allSizes = [None]
-    for i in allSizes:
-        if not i % 100:
-            print "Starting loop for sample of size %d" % i
-        relativeEntropy = 0
-        for j in range(numRuns):
-            transitionMatrix = assignNewTrajecories(X, lagtime, i)
-            try:
-                relativeEntropy += markov.getRelativeEntropy(refStationaryDist, refTransition, transitionMatrix)
-            except:
-                j -= 1
-        entropies.append(relativeEntropy/float(numRuns))
-    if i:
-        plt.plot(allSizes, entropies)
+        numberOfTrajs = [None]
+        allTrajLengths = [None]
+
+    for length in allTrajLengths:
+        if length: print "Working with trajectories of length: %d" % length
+        lengthEntropies = []
+        for ntrajs in numberOfTrajs:
+            if not ntrajs % 25:
+                print "Starting loop for sample of %d trajs" % ntrajs
+            relativeEntropy = 0
+            for j in range(numRuns):
+                transitionMatrix = assignNewTrajecories(X, goldenMSMClusterCenters, lagtime, ntrajs, length)
+                try:
+                    s = markov.getRelativeEntropy(refStationaryDist, refTransition, transitionMatrix)
+                    relativeEntropy += s
+                except ValueError:
+                    j -= 1
+            lengthEntropies.append(relativeEntropy/float(numRuns))
+        entropies.append(lengthEntropies)
+
+    np.save("matrix_1.npy", entropies)
+
+    for i, length in enumerate(allTrajLengths):
+        for j, ntrajs  in enumerate(numberOfTrajs):
+            print length, ntrajs, entropies[i][j]
+        print ""
+
+    if ntrajs and length:
+        plt.figure(1)
+        plt.imshow(entropies, interpolation="nearest", origin="lower", aspect="auto", extent=[numberOfTrajs[0], numberOfTrajs[-1], allTrajLengths[0], allTrajLengths[-1]])
+        #plt.imshow(entropies, interpolation="nearest", extent=[numberOfTrajs[0], numberOfTrajs[-1], 800, 801])
+        plt.colorbar()
+        plt.figure(2)
+        plt.imshow(entropies, interpolation="bilinear", origin="lower", aspect="auto",  extent=[numberOfTrajs[0], numberOfTrajs[-1], allTrajLengths[0], allTrajLengths[-1]])
+        plt.colorbar()
+        plt.show()
+    elif ntrajs:
+        print numberOfTrajs, entropies[0]
+        plt.plot(numberOfTrajs, entropies[0])
         plt.show()
     else:
         print entropies
