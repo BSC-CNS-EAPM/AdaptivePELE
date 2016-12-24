293445b1d47b0e3016704f5fcd44c20b336ffe92
(D. Lecina) <(danilecina@gmail.com)>
2016-12-24 20:03:02 +0100

    Improved compute delta g script. Now it generates pdb with cluster centers and gpmf, it no longer requires allPoints file and other improvements

--- pyemma_scripts/computeDeltaG.py
+++ pyemma_scripts/computeDeltaG.py
@@ -1,7 +1,32 @@
 import numpy as np
 import helper
 import glob
+import sys
+import argparse
 from scipy.ndimage import filters
+
+def parseArgs():
+    parser = argparse.ArgumentParser(description="Script that computes delta G")
+    parser.add_argument('trajectory', type=str)
+    args = parser.parse_args()
+    return args.trajectory
+
+def writePDB(pmf_xyzg, title="clusters.pdb"):
+    templateLine = "HETATM%s  H1  CLT L 502    %s%s%s  0.75%s           H\n"
+
+    content = ""
+    for i, line in enumerate(pmf_xyzg):
+        number = str(i).rjust(5)
+        x = ("%.3f"%line[0]).rjust(8)
+        y = ("%.3f"%line[1]).rjust(8)
+        z = ("%.3f"%line[2]).rjust(8)
+        g = ("%.3f"%line[3]).rjust(8)
+
+        content += templateLine%(number, x, y, z, g)
+    f = open(title, 'w')
+    f.write(content)
+    f.close()
+
 
 clusteringObject = helper.loadMSM('clustering_object.pkl')
 r = clusteringObject.clustercenters
@@ -15,19 +40,31 @@ pi = MSMObject.stationary_distribution
 #r = data[:,0:3]
 #pi = data[:,3]
 
-#d = 0.05
-d = 0.25
+d = 1.0
 
-allPointsFilename = "test/MSM1/allPoints_1.dat"
-allPoints = np.loadtxt(allPointsFilename)
-maxval = allPoints.max(axis=0)
-minval = allPoints.min(axis=0)
+trajWildcard = parseArgs()
+originalFilenames = glob.glob(trajWildcard)
+
+originalCoordinates = []
+for i, originalFilename in enumerate(originalFilenames):
+    trajOriginalCoordinates = np.loadtxt(originalFilename, usecols=(1,2,3))
+    originalCoordinates.append(trajOriginalCoordinates)
+
+maxval = 3*[-np.inf]
+minval = 3*[np.inf]
+for coord in originalCoordinates:
+    cmaxval = coord.max(axis=0)
+    cminval = coord.min(axis=0)
+    maxval = np.maximum(cmaxval, maxval)
+    minval = np.minimum(cminval, minval)
+
+print "Maximum bounds", maxval, "Minimum bounds", minval
 
 #Rounded floor and ceiling in intervals of "d" (e.g., floor of 1.73 with d = 0.5, will be 1.5 instead of 1.0, in order to optimize box creation.
 #An extra box is included in the ceiling, so that all the points are contained in the range given by arange
-bins = [np.arange(np.floor(minval[i]) + d*int((minval[i] - np.floor(minval[i]))/d),
+bins = np.array([np.arange(np.floor(minval[i]) + d*int((minval[i] - np.floor(minval[i]))/d),
                     np.ceil(maxval[i]) + d*(int((maxval[i] - np.ceil(maxval[i]))/d) + 1),
-        d) for i in range(3)]
+        d) for i in range(3)])
 
 """
 bins = [np.arange(np.floor(minval[i]),
@@ -37,26 +74,23 @@ bins = [np.arange(np.floor(minval[i]),
 
 
 numberOfClusters = r.shape[0]
-originalFilenames = glob.glob("test/MSM1/traj_*")
 histogram = np.array([])
+histogramFreq = np.array([])
+histograms = []
+
 microstateVolume = np.zeros(numberOfClusters)
 
 print "Number of clusters", numberOfClusters
 
-originalCoordinates = []
-for originalFilename in originalFilenames:
-    trajOriginalCoordinates = np.loadtxt(originalFilename, usecols=(1,2,3))
-    originalCoordinates.append(trajOriginalCoordinates)
 
 
 for i in range(numberOfClusters):
-#for i in range(2):
     allCoords = []
-    for trajOriginalCoordinates, dtraj in zip(originalCoordinates, clusteringObject.dtrajs):
+    for j,(trajOriginalCoordinates, dtraj) in enumerate(zip(originalCoordinates, clusteringObject.dtrajs)):
+        assert dtraj.shape[0] == trajOriginalCoordinates.shape[0]
         belongingFrames = np.argwhere(dtraj==i)
         trajCoords = trajOriginalCoordinates[belongingFrames, :]
         trajCoords = trajCoords.flatten().tolist()
-
 
         """
         if allCoords.size == 0:
@@ -68,17 +102,29 @@ for i in range(numberOfClusters):
 
     allCoords = np.reshape(allCoords, (-1,3))
 
-    current_hist, edges = np.histogramdd(allCoords, bins=bins, normed=True)
+    current_hist, edges = np.histogramdd(allCoords, bins=bins)
+    histograms.append(current_hist)
 
-    filtered_hist = filters.gaussian_filter(current_hist, sigma=1)
+    #filtered_hist = filters.gaussian_filter(current_hist, sigma=1)
+    filtered_hist = current_hist/current_hist.sum()
 
-    microstateVolume[i] = len(np.argwhere(filtered_hist > 0)) * d**3
-    print i, microstateVolume[i]
+    nonZeroIndices = np.argwhere(filtered_hist > 0)
+
+    # microstateVolume[i] = len(nonZeroIndices) * d**3
 
     if histogram.size == 0:
-        histogram = pi[i]*current_hist
+        histogramFreq = pi[i]*filtered_hist
+        histogram = np.copy(current_hist)
     else:
-        histogram += pi[i]*current_hist
+        histogramFreq += pi[i]*filtered_hist
+        histogram += current_hist
+
+
+for i in range(numberOfClusters):
+    histogramCluster = histograms[i]
+    histogramTotal = histogram[histogramCluster > 0]
+    histogramCluster = histogramCluster[histogramCluster > 0]
+    microstateVolume[i] = (histogramCluster/histogramTotal).sum() * d**3
 
 
 np.savetxt("volumeOfClusters.dat", microstateVolume)
@@ -89,34 +135,70 @@ kb = 0.0019872041
 T = 300
 beta = 1/(kb*T)
 
-"""
 gpmf = -kb*T*np.log(pi/microstateVolume)
 gpmf -= gpmf.min()
+#debug
+#for i,j in enumerate(gpmf):
+#    print i, j, pi[i], microstateVolume[i]
 
-bindingVolume = 0
-for g, volume in zip(gpmf, microstateVolume):
-   bindingVolume += np.exp(-beta * g) * volume 
+deltaW = -gpmf[gpmf != np.inf].max()
+print "bound    Delta G     Delta W     Binding Volume:     Binding Volume contribution"
 
-deltaW = -gpmf.max()
+upperGpmfValues = np.arange(0,-deltaW,0.5)
 
-print r.shape, gpmf.shape
-np.savetxt("pmf_xyzg.dat", np.hstack((r, np.expand_dims(gpmf,axis=1))))
+for upperGpmfValue in upperGpmfValues:
+    bindingVolume = 0
+    for g, volume in zip(gpmf, microstateVolume):
+        if g <= upperGpmfValue:
+            bindingVolume += np.exp(-beta * g) * volume 
+    deltaG = deltaW - kb*T*np.log(bindingVolume/1661)
+    print "%.1f\t%.3f\t%.3f\t%.3f\t%.3f" % (upperGpmfValue, deltaG, deltaW, bindingVolume, -kb*T*np.log(bindingVolume/1661))
 
+
+pmf_xyzg = np.hstack((r, np.expand_dims(gpmf,axis=1)))
+np.savetxt("pmf_xyzg.dat", pmf_xyzg)
+
+writePDB(pmf_xyzg)
+
+
+sys.exit()
 """
+"""
+
 gpmf = -kb*T*np.log(histogram)
 gpmf -= gpmf.min()
 
 #inExplorationRange = np.argwhere(gpmf != np.inf)
 deltaW = -gpmf[gpmf != np.inf].max()
 
-bindingVolume = 0
-for i,g in np.ndenumerate(gpmf):
-    bindingVolume += np.exp(-beta*g)
-bindingVolume *= d**3
+for indices in np.argwhere(gpmf != np.inf):
+    i = indices[0]
+    j = indices[1]
+    k = indices[2]
+    #print bins[0][i], bins[1][j], bins[2][k], gpmf[i,j,k]
 
-deltaG = deltaW - kb*T*np.log(bindingVolume/1661)
-
-#np.savetxt("pmf_xyzg.dat", np.hstack((r, np.expand_dims(gpmf,axis=1))))
-
+upperGpmfValues = np.arange(0,-deltaW,0.5)
+bindingVolumes = []
+deltaGs = []
 print "Delta G     Delta W     Binding Volume:     Binding Volume contribution"
-print "%.3f\t%.3f\t%.3f\t%.3f" % (deltaG, deltaW, bindingVolume, -kb*T*np.log(bindingVolume/1661))
+for upperGpmfValue in upperGpmfValues:
+    bindingVolume = 0
+    for i,g in np.ndenumerate(gpmf[gpmf <= upperGpmfValue]):
+        bindingVolume += np.exp(-beta*g)
+    bindingVolume *= d**3
+
+    deltaG = deltaW - kb*T*np.log(bindingVolume/1661)
+
+    deltaGs.append(deltaG)
+    bindingVolumes.append(bindingVolume)
+
+    #np.savetxt("pmf_xyzg.dat", np.hstack((r, np.expand_dims(gpmf,axis=1))))
+
+    print "%.3f\t%.3f\t%.3f\t%.3f" % (deltaG, deltaW, bindingVolume, -kb*T*np.log(bindingVolume/1661))
+
+import matplotlib.pyplot as plt
+plt.figure(1)
+plt.plot(upperGpmfValues, deltaGs)
+plt.figure(2)
+plt.plot(upperGpmfValues, bindingVolumes)
+plt.show()
