307aa7726d6316f2b2ed2fc3121207bb401300e6
(D. Lecina) <(danilecina@gmail.com)>
2016-12-29 11:35:57 +0100

    Commented out PCCA and TPT from pyemma, added regularSpace clustering and writeClusterToPDB(still not tested)

--- pyemma_scripts/msm.py
+++ pyemma_scripts/msm.py
@@ -1,10 +1,10 @@
-import pyemma.msm as msm
+import pyemma.msm as MSM
 import pyemma.plots as mplt
 import matplotlib.pyplot as plt
 
 def calculateITS(trajectories, lagtimes, errors = None):
     """ Calulate the implied time-scales at the given lagtimes"""
-    its_object = msm.its(trajectories, lags=lagtimes, errors=errors)
+    its_object = MSM.its(trajectories, lags=lagtimes, errors=errors)
     return its_object
 
 def plotITS(its_object, its_plot_file=None, nits=-1):
@@ -15,7 +15,7 @@ def estimateMSM(trajectories,lagtime):
     """ Estimate a MSM from the trajectories using a provided lagtime that
     should be big enough so that the relevant processes have converged.
     Return a MaximumLikelihoodMSM object"""
-    MSM_object = msm.estimate_markov_model(trajectories, lagtime)
+    MSM_object = MSM.estimate_markov_model(trajectories, lagtime)
     return MSM_object
 
 def calculatePCCA(MSM_object, numPCCA):
@@ -26,7 +26,7 @@ def calculatePCCA(MSM_object, numPCCA):
 
 def is_connected(MSM_object):
     """ Check if the MSM created is connected"""
-    return msm.analysis.is_connected(MSM_object.transition_matrix)
+    return MSM.analysis.is_connected(MSM_object.transition_matrix)
 
 def ChapmanKolmogorovTest(MSM_object, nsets,memberships=None, error_estimation=False, mlags=2):
     """ Perform the ChapmanKolmogorov test to validate the MSM"""
