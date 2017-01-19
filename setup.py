7bd0e17b89e50e63483836fff37a7bdc1149b004
(D. Lecina) <(danilecina@gmail.com)>
2017-01-19 15:37:17 +0100

    Changes how the integration tests work

---
+++ setup.py
@@ -0,0 +1,16 @@
+import numpy
+from distutils.core import setup
+from Cython.Build import cythonize
+from distutils.extension import Extension
+
+
+ext_modules = [
+        Extension("atomset/atomset", ["atomset/atomset.pyx"], include_dirs = [".", "atomset"]),
+        Extension("atomset/SymmetryContactMapEvaluator", ["atomset/SymmetryContactMapEvaluator.pyx"], include_dirs = [".","atomset"]),
+        Extension("atomset/RMSDCalculator", ["atomset/RMSDCalculator.pyx"], include_dirs = [".", "atomset"])
+            ]
+setup(
+    ext_modules = cythonize(ext_modules),  # accepts a glob pattern
+    include_dirs=[numpy.get_include()]
+
+)
--- tests/testAdaptiveSampling.py
+++ tests/testAdaptiveSampling.py
@@ -2,23 +2,33 @@ import adaptiveSampling
 import unittest
 import pickle
 from atomset import atomset
+from clustering import clustering
 import shutil
 import os
 
 
 class TestadaptiveSampling(unittest.TestCase):
 
-    def checkClusteringObjects(self, goldenPath, outputPath):
-        goldenPathObject = os.path.join(goldenPath, "%d/clustering/object.pkl")
+    def checkClusteringObjects(self, goldenClusters, outputPath):
+        # goldenPathObject = os.path.join(goldenPath, "%d/clustering/object.pkl")
         outputPathObject = os.path.join(outputPath, "%d/clustering/object.pkl")
         for i in range(2, 3):
             with open(outputPathObject % i, 'rb') as f2:
                 outputCluster = pickle.load(f2)
             outputCluster.clusters.printClusters()
-            with open(goldenPathObject % i, 'rb') as f:
-                goldenCluster = pickle.load(f)
-            goldenCluster.clusters.printClusters()
-            self.assertEqual(outputCluster, goldenCluster)
+            # with open(goldenPathObject % i, 'rb') as f:
+            #     goldenCluster = pickle.load(f)
+            # goldenCluster.clusters.printClusters()
+            self.assertEqual(outputCluster.clusters.getNumberClusters(), len(goldenClusters))
+            for goldCl, outCl in zip(goldenClusters, outputCluster.clusters.clusters):
+                outCl.printCluster()
+                goldCl.printCluster()
+                print type(goldCl.pdb)
+                print type(goldCl.pdb.pdb)
+                print type(outCl.pdb)
+                print type(outCl.pdb.pdb)
+                print len(outCl.pdb.pdb), len(goldCl.pdb.pdb)
+                self.assertEqual(outCl, goldCl)
 
     def checkStartingConformations(self, goldenPath, outputPath):
         goldenPathInitial = os.path.join(goldenPath, "%d/initial_%d.pdb")
@@ -55,14 +65,14 @@ class TestadaptiveSampling(unittest.TestCase):
 
                 self.assertEqual(goldenTraj, outputTraj)
 
-    def integrationTest(self, controlFile, goldenPath, outputPath):
+    def integrationTest(self, controlFile, goldenPath, outputPath, goldenClusters):
         # Function to test --> integration test
         adaptiveSampling.main(controlFile)
 
         # Assertions
         tmpFolder = "tmp_" + outputPath.replace("/", "_")
 
-        self.checkClusteringObjects(goldenPath, outputPath)
+        self.checkClusteringObjects(goldenClusters, outputPath)
         self.checkStartingConformations(goldenPath, tmpFolder)
         self.checkTrajectories(goldenPath, outputPath)
 
@@ -77,29 +87,58 @@ class TestadaptiveSampling(unittest.TestCase):
         controlFile = "tests/data/3ptb_data/integrationTest1.conf"
         goldenPath = "tests/data/3ptb_data/originTest1"
         outputPath = "tests/data/3ptb_data/Test1"
+        elements = [28, 17, 5, 18, 1, 3]
+        goldenClusters = []
+        for i in range(6):
+            pdb = atomset.PDB()
+            pdb.initialise(goldenPath+"/2/clustering/cluster_%d.pdb" % i, resname="AEN")
+            cluster = clustering.Cluster(pdb, thresholdRadius=4)
+            cluster.elements = elements[i]
+            cluster.contacts = 0
+            goldenClusters.append(cluster)
+        self.integrationTest(controlFile, goldenPath, outputPath, goldenClusters)
 
-        self.integrationTest(controlFile, goldenPath, outputPath)
-
-    def testIntegration2(self):
+    def _testIntegration2(self):
         """
             Simulations are not run, trajectories and reports are copied
         """
         controlFile = "tests/data/3ptb_data/integrationTest2.conf"
         goldenPath = "tests/data/3ptb_data/srcTest2Epsilon"
         outputPath = "tests/data/3ptb_data/Test2"
+        pdbs = [open(goldenPath+"/2/clustering/cluster_%d.pdb" % i).read() for i in range(5)]
+        elements = [35, 14, 8, 14, 1]
+        metrics = [[1.0, 0.0, 0.0, -7498.07, 20.1909, 0.216436],
+                   [1.0, 0.0, 0.0, -7498.06, 18.5232, 0.229384],
+                   [1.0, 0.0, 0.0, -7498.01, 18.6059, 0.253929],
+                   [1.0, 0.0, 0.0, -7498.05, 22.4539, 0.238184],
+                   [1.0, 4.0, 4.0, -7498.05, 22.7766, 0.242335]
+                  ]
+        goldenClusters = []
+        for i in range(5):
+            cluster = clustering.Cluster(pdbs[i], 4, metrics=metrics[i])
+            cluster.elements = elements[i]
+            cluster.contacts = 0
+            goldenClusters.append(cluster)
 
-        self.integrationTest(controlFile, goldenPath, outputPath)
+        self.integrationTest(controlFile, goldenPath, outputPath, goldenClusters)
 
-    def testIntegration3(self):
+    def _testIntegration3(self):
         """
             Simulations are actually run
         """
         controlFile = "tests/data/3ptb_data/integrationTest3.conf"
         goldenPath = "tests/data/3ptb_data/originTest3"
         outputPath = "tests/data/3ptb_data/Test3"
-
+        pdbs = [open(goldenPath+"/2/clustering/cluster_%d.pdb" % i).read() for i in range(4)]
+        elements = [27, 22, 21, 2]
+        goldenClusters = []
+        for i in range(4):
+            cluster = clustering.Cluster(pdbs[i], 4)
+            cluster.elements = elements[i]
+            cluster.contacts = 0
+            goldenClusters.append(cluster)
         try:
-            self.integrationTest(controlFile, goldenPath, outputPath)
+            self.integrationTest(controlFile, goldenPath, outputPath, goldenClusters)
         except SystemExit:
             # Catch error for not having PELE installed
             print ("Warning! There was a sysExit in test3, this is usually "
