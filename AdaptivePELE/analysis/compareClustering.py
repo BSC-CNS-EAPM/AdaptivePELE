2ba1631abe29ec3809e544232bd655f1552d1bb0
jgilaber <joan.gilabert@bsc.es>
2017-02-21 15:29:26 +0100

    Alternative structures are stores in a priority queue wrt to the metric and randomly selected at spawn time

--- AdaptivePELE/analysis/compareClustering.py
+++ AdaptivePELE/analysis/compareClustering.py
@@ -1,4 +1,4 @@
-from ApativePELE.clustering import clustering, thresholdcalculator
+from AdaptivePELE.clustering import clustering, thresholdcalculator
 import time
 # import pickle
 # import pdb as debug
@@ -83,7 +83,8 @@ densityCalculator = densityCalculatorBuilder.build({
 # densityCalculator = densityCalculatorBuilder.build({})
 contactThresholdDistance = 8
 resname = "STR"
-nEpochs = 5
+nEpochs = 2
+altSel = True
 ClDouble = clustering.ContactMapClustering(resname=resname,
                                            reportBaseFilename="report",
                                            columnOfReportFile=5,
@@ -96,24 +97,25 @@ ClCont = clustering.ContactsClustering(thresholdCalculator, resname=resname,
                                        reportBaseFilename="report",
                                        columnOfReportFile=4,
                                        contactThresholdDistance=contactThresholdDistance,
-                                       symmetries=[])
+                                       symmetries=[], altSelection=altSel)
 ClAcc = clustering.ContactMapAccumulativeClustering(thresholdCalculatorAcc,
                                                     similarityEvaluator,
                                                     resname=resname,
                                                     reportBaseFilename="report",
                                                     columnOfReportFile=5,
-                                                    contactThresholdDistance=contactThresholdDistance)
+                                                    contactThresholdDistance=contactThresholdDistance,
+                                                    altSelection=altSel)
 # spawningObject = spawning.InverselyProportionalToPopulationCalculator(densityCalculator)
 spawningObject = spawning.UCBCalculator(densityCalculator)
 for i in range(nEpochs):
     # path =["trajs/%d/run_traj*"%i]
     # paths_report = ["trajs/%d/run_report*"%i]
-    path = ["/home/bsc72/bsc72021/simulations/PR/testCM_4_32/simulation/PRprog_CM_variabExtra_UCB_5/%d/traj*" % i]
-    paths_report = ["/home/bsc72/bsc72021/simulations/PR/testCM_4_32/simulation/PRprog_CM_variabExtra_UCB_5//%d/report*" % i]
+    # path = ["/home/bsc72/bsc72021/simulations/PR/testCM_4_32/simulation/PRprog_CM_variabExtra_UCB_5/%d/traj*" % i]
+    # paths_report = ["/home/bsc72/bsc72021/simulations/PR/testCM_4_32/simulation/PRprog_CM_variabExtra_UCB_5//%d/report*" % i]
     # path = ["/gpfs/scratch/bsc72/bsc72021/AdaptiveCM/simulation/PRprog_4_64CMExtra/automate_CM_7/%d/traj*"%i]
     # paths_report = ["/gpfs/scratch/bsc72/bsc72021/AdaptiveCM/simulation/PRprog_4_64CMExtra/automate_CM_7/%d/report*"%i]
-    # path = ["/home/jgilaber/PR/PRprog_4_64CMExtraSubset_thres/automate_CM_3/%d/traj*"%i]
-    # paths_report = ["/home/jgilaber/PR/PRprog_4_64CMExtraSubset_thres/automate_CM_3/%d/report*"%i]
+    path = ["/home/jgilaber/PR/PRprog_4_64CMExtraSubset_thres/automate_CM_3/%d/traj*"%i]
+    paths_report = ["/home/jgilaber/PR/PRprog_4_64CMExtraSubset_thres/automate_CM_3/%d/report*"%i]
     # path = ["/home/jgilaber/4DAJ/4DAJ_4_64CMExtraSubsetSingle/%d/traj*"%i]
     # paths_report = ["/home/jgilaber/4DAJ/4DAJ_4_64CMExtraSubsetSingle/%d/report*"%i]
     trajs = clustering.getAllTrajectories(paths_report)
@@ -123,14 +125,14 @@ for i in range(nEpochs):
             total_snapshots += 1
         total_snapshots -= 1
     print "Total snapsthots for epoch %d: %d" % (i, total_snapshots)
-    # startTimeCont = time.time()
-    # ClCont.cluster(path)
-    # endTimeCont = time.time()
-    # print "Total time of clustering contacts, epoch %d: %.6f"%(i,endTimeCont-startTimeCont)
-    # print "Number of clusters contacts epoch %d: %d"%(i,len(ClCont.clusters.clusters))
-    # degeneraciesCont = spawningObject.calculate(ClCont.clusters.clusters, ntrajs, {})
-    # ClCont.writeOutput("clsummary",degeneraciesCont,"object_Cont.pkl", False)
-    # os.rename("clsummary/summary.txt", "results/summary_ClCont.txt")
+    startTimeCont = time.time()
+    ClCont.cluster(path)
+    endTimeCont = time.time()
+    print "Total time of clustering contacts, epoch %d: %.6f"%(i,endTimeCont-startTimeCont)
+    print "Number of clusters contacts epoch %d: %d"%(i,len(ClCont.clusters.clusters))
+    degeneraciesCont = spawningObject.calculate(ClCont.clusters.clusters, ntrajs, {})
+    ClCont.writeOutput("clsummary",degeneraciesCont,"object_Cont_test.pkl", False)
+    os.rename("clsummary/summary.txt", "results/summary_ClCont.txt")
     # startTimeDouble = time.time()
     # ClDouble.cluster(path)
     # endTimeDouble = time.time()
@@ -148,16 +150,16 @@ for i in range(nEpochs):
     # degeneraciesAgg = spawningObject.calculate(ClAgg.clusters.clusters, ntrajs, {})
     # ClAgg.writeOutput("clsummary",degeneraciesAgg,"ClAgg.pkl", False)
     # os.rename("clsummary/summary.txt", "results/summary_ClAgg.txt")
-    startTimeAcc = time.time()
-    ClAcc.cluster(path)
-    endTimeAcc = time.time()
-    print "Total time of clustering accumulative, epoch %d: %.6f" % (i,endTimeAcc-startTimeAcc)
-    print "Number of clusters accumulative epoch %d: %d" % (i,len(ClAcc.clusters.clusters))
-    print ""
-    degeneraciesAcc = spawningObject.calculate(ClAcc.clusters.clusters, ntrajs, {})
-    summaryFolder = "%d/clustering" % i
-    if not os.path.exists(summaryFolder):
-        os.makedirs(summaryFolder)
-    ClAcc.writeOutput(summaryFolder, degeneraciesAcc, "ClAcc.pkl", False)
+    # startTimeAcc = time.time()
+    # ClAcc.cluster(path)
+    # endTimeAcc = time.time()
+    # print "Total time of clustering accumulative, epoch %d: %.6f" % (i,endTimeAcc-startTimeAcc)
+    # print "Number of clusters accumulative epoch %d: %d" % (i,len(ClAcc.clusters.clusters))
+    # print ""
+    # degeneraciesAcc = spawningObject.calculate(ClAcc.clusters.clusters, ntrajs, {})
+    # summaryFolder = "%d/clustering" % i
+    # if not os.path.exists(summaryFolder):
+    #     os.makedirs(summaryFolder)
+    # ClAcc.writeOutput(summaryFolder, degeneraciesAcc, "ClAcc_testAlt.pkl", False)
     # os.remove(os.path.join(summaryFolder, "ClAcc.pkl"))
     # os.rename("clsummary/summary.txt", "%d/clustering/summary_ClAcc_set1.txt" % i)
