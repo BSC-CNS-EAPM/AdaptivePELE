e00da493cb83185e1985f0cc3a0bd796be423aa9
jgilaber <joan.gilabert@bsc.es>
2018-01-17 17:32:06 +0100

    Add the possibility to filter unbindin events with thee foldersFirstBindingEvent script

--- AdaptivePELE/analysis/foldersFirstBindingEvent.py
+++ AdaptivePELE/analysis/foldersFirstBindingEvent.py
@@ -18,13 +18,14 @@ def parseArguments():
     parser.add_argument("threshold", type=float, help="Threshold for a binding event to be considered")
     parser.add_argument("stepsPerEpoch", type=int, help="StepsPerEpoch")
     parser.add_argument("-seq", action='store_true', help="Use a sequential run, instead of adaptive")
+    parser.add_argument("-u", action='store_true', help="Look for unbinding event, instead of binding")
     parser.add_argument("folders", nargs='+', default=".",  help="Folders with adaptive sampling runs")
     args = parser.parse_args()
 
-    return  args.folders, args.column, args.threshold, args.stepsPerEpoch, args.seq
+    return  args.folders, args.column, args.threshold, args.stepsPerEpoch, args.seq, args.u
 
 
-def main(folders, column, threshold, stepsPerEpoch, sequential):
+def main(folders, column, threshold, stepsPerEpoch, sequential, unbinding):
     """
         Calculate first binding event statistics (mean, median, std)
 
@@ -40,17 +41,20 @@ def main(folders, column, threshold, stepsPerEpoch, sequential):
             sequential simulation
         :type sequential: bool
     """
+    print_label = "binding"
+    if unbinding:
+        print_label = "unbinding"
     firstBE = []
     for folder in folders:
         cwd = os.getcwd()
         os.chdir(folder)
         if sequential:
-            stepsFirstBindingEvent = findfirstbindingevent.findEpochFirstBindingEvent(threshold, column)
+            stepsFirstBindingEvent = findfirstbindingevent.findEpochFirstBindingEvent(threshold, column, unBinding=unbinding)
         else:
-            stepsFirstBindingEvent = findfirstbindingevent.findFirstBindingEvent(stepsPerEpoch, column, threshold)
+            stepsFirstBindingEvent = findfirstbindingevent.findFirstBindingEvent(stepsPerEpoch, column, threshold, unBinding=unbinding)
 
-        if not stepsFirstBindingEvent:
-            print "Didn't see any first binding event in folder:", folder
+        if stepsFirstBindingEvent is None:
+            print "Didn't see any first %s event in folder:" % print_label, folder
         else:
             firstBE.append(stepsFirstBindingEvent)
 
@@ -59,10 +63,12 @@ def main(folders, column, threshold, stepsPerEpoch, sequential):
     if len(firstBE) > 1:
         print firstBE
         analyse.analyseData(firstBE)
-    else:
+    elif len(firstBE) == 1:
         print firstBE[0]
+    else:
+        print "No %s event found" % print_label
 
 
 if __name__ == "__main__":
-    folders, column, threshold, stepsPerEpoch, seq = parseArguments()
-    main(folders, column, threshold, stepsPerEpoch, seq)
+    folders, column, threshold, stepsPerEpoch, seq, unbind = parseArguments()
+    main(folders, column, threshold, stepsPerEpoch, seq, unbind)
