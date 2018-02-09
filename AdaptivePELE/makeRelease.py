866f3fb597a620e98bf3f3553422474a276ca0cf
jgilaber <joan.gilabert@bsc.es>
2018-02-09 15:00:01 +0100

    Improve makeRelease script to avoid copying .so and .c files

--- AdaptivePELE/makeRelease.py
+++ AdaptivePELE/makeRelease.py
@@ -7,6 +7,10 @@ import glob
 import shutil
 import os
 
+def copy_ignore(src, names):
+    return filter(lambda x: x.endswith(".c") or x.endswith(".so"), names)
+
+
 machine = socket.gethostname()
 if "bsccv" in machine:
     releaseFolder = "/data2/bsc72/AdaptiveSampling/bin"
@@ -17,21 +21,20 @@ elif 'login' in machine:
     elif name == "nord3":
         releaseFolder = "/gpfs/projects/bsc72/adaptiveSampling/bin_nord"
 
-releaseName = "v1.4"
+releaseName = "v1.4.1"
 toOmit = ["tests", "runAllTests.py", "os", "sys", "TODO.txt", "Data", "Documents", "DataLocal", "epsilon_values.txt", "makeRelease.py", ".git", ".gitignore"]
 
 
 files = glob.glob("*")
 
 destFolder = os.path.join(releaseFolder, releaseName, "AdaptivePELE", "%s")
-print destFolder
 for filename in files:
     if filename in toOmit or filename.startswith(".") or filename.endswith("pyc"):
         continue
     print "Copying", filename
     try:
         if not os.path.exists(destFolder % filename):
-            shutil.copytree(filename, destFolder % filename)
+            shutil.copytree(filename, destFolder % filename, ignore=copy_ignore)
     except (IOError, OSError):
         shutil.copyfile(filename, destFolder % filename)
 
