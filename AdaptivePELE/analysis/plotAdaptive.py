bf67fe5318ace4275c4665b025e0314a3cbf24a1
jgilaber <joan.gilabert@bsc.es>
2018-03-12 17:54:21 +0100

    Refactor plotAdaptive + add possibility of using a metric as color

--- AdaptivePELE/analysis/plotAdaptive.py
+++ AdaptivePELE/analysis/plotAdaptive.py
@@ -1,7 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
 import os
 import argparse
 import glob
-import types
 
 
 def parseArguments():
@@ -23,11 +23,12 @@ def parseArguments():
     parser.add_argument("filename", type=str, default="report_", help="Report filename")
     parser.add_argument("-be", action="store_true", help="Points")
     parser.add_argument("-rmsd", action="store_true", help="Lines")
+    parser.add_argument("-zcol", type=int, default=None, help="Column to define color according to metric")
     args = parser.parse_args()
-    return args.steps, args.xcol, args.ycol, args.filename, args.be, args.rmsd
+    return args.steps, args.xcol, args.ycol, args.filename, args.be, args.rmsd, args.zcol
 
 
-def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps=False, replotFirst=False):
+def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps=False, replotFirst=False, paletteModifier=None):
     """
         Generate a string to be passed to gnuplot
 
@@ -46,6 +47,8 @@ def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRu
         :type TotalNumberOfSteps: bool
         :param replotFirst: Deprecated parameter
         :type replotFirst: bool
+        :param paletteModifier: Wheter to use the epoch as color or a column
+        :type paletteModifier: int
 
         :returns: str -- String to plot using gnuplot
     """
@@ -62,7 +65,7 @@ def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRu
     if printWithLines:
         dictionary['withLines'] = "w l"
 
-    if isinstance(column1, types.IntType):
+    if isinstance(column1, int):
         if totalNumberOfSteps:
             dictionary['col1'] = "($" + str(column1) + "+ (%d*j))" % stepsPerRun  # adds steps per runs, so that it mathes the total number of steps
         else:
@@ -71,63 +74,7 @@ def generateNestedString(gnuplotString, reportName, column1, column2, stepsPerRu
     return gnuplotString % dictionary + "\n"
 
 
-def generateForLoopString(gnuplotString, reportName, column1, column2, stepsPerRun, printWithLines, totalNumberOfSteps=False, replotFirst=False):
-    """
-        Generate a string to be passed to gnuplot
-
-        :param gnuplotString: Template string for gnuplot
-        :type gnuplotString: str
-        :param reportName: Name of the files containing the simulation data
-        :type reportName: str
-        :param column1: Column to plot in the X axis
-        :type column1: int
-        :param column2: Column to plot in the Y axis
-        :type column2: int
-        :param stepsPerRun: Number of steps per epoch,
-        :type stepsPerRun: int
-        :param printWithLines:  Wether the plot should use solid lines
-        :type printWithLines: bool
-        :param TotalNumberOfSteps: Not only considering steps in current epoch,
-            but steps with all previous epochs
-        :type TotalNumberOfSteps: bool
-        :param replotFirst:
-        :type replotFirst: bool
-
-        :returns: str -- String to plot using gnuplot
-    """
-    allFolders = os.listdir('.')
-    epochFolders = [epoch for epoch in allFolders if epoch.isdigit()]
-    numberOfEpochs = int(len(epochFolders))
-
-    dictionary = {'reportName': reportName, 'col2': column2, 'numberOfEpochs': numberOfEpochs, 'withLines': ''}
-    plottingString = ""
-    for i in range(numberOfEpochs):
-        dictionary['epoch'] = i
-
-        numberOfRunsPerEpoch = len(glob.glob(os.path.join(str(i), reportName+"*")))
-        dictionary['runsPerEpoch'] = numberOfRunsPerEpoch
-
-        if printWithLines:
-            dictionary['withLines'] = "w l"
-
-        if isinstance(column1, types.IntType):
-            if totalNumberOfSteps:
-                dictionary['col1'] = "($" + str(column1) + "+" + str(stepsPerRun*i) + ")"  # adds steps per runs, so that it mathes the total number of steps
-            else:
-                dictionary['col1'] = str(column1)
-        elif isinstance(column1, types.LambdaType):
-            dictionary['col1'] = "(" + str(column1(i)) + ")"
-
-        if i != 0 or replotFirst:
-            plottingString += "re"
-
-        plottingString += gnuplotString % dictionary + "\n"
-        # plottingString += "pause 0.25\n"
-
-    print plottingString
-
-
-def generatePrintString(stepsPerRun, xcol, ycol, reportName, kindOfPrint):
+def generatePrintString(stepsPerRun, xcol, ycol, reportName, kindOfPrint, paletteModifier):
     """
         Generate a template string to use with gnuplot
 
@@ -150,18 +97,24 @@ def generatePrintString(stepsPerRun, xcol, ycol, reportName, kindOfPrint):
     elif kindOfPrint == "PRINT_BE_RMSD":
         printWithLines = False
         totalNumberOfSteps = False
+    if paletteModifier is None:
+        stringPalette = "frac j/%(numberOfEpochs)d. "
+        colorMetric = ""
+    else:
+        stringPalette = ""
+        colorMetric = ":%d" % paletteModifier
 
-    gnuplotString = "plot for [i=1:%(runsPerEpoch)d] for [j=0:%(numberOfEpochs)d-1] \'\'.j.\'/%(reportName)s\'.i u %(col1)s:%(col2)d lt 6 lc palette frac j/%(numberOfEpochs)d. notitle %(withLines)s"
-    return generateNestedString(gnuplotString, reportName, xcol, ycol, stepsPerRun, printWithLines, totalNumberOfSteps, False)
+    gnuplotString = "".join(["plot for [i=1:%(runsPerEpoch)d] for [j=0:%(numberOfEpochs)d-1] \'\'.j.\'/%(reportName)s\'.i u %(col1)s:%(col2)d", colorMetric, " lt 6 lc palette ", stringPalette, "notitle %(withLines)s"])
+    return generateNestedString(gnuplotString, reportName, xcol, ycol, stepsPerRun, printWithLines, totalNumberOfSteps, False, paletteModifier)
 
 
 if __name__ == "__main__":
-    steps_Run, Xcol, Ycol, filename, be, rmsd = parseArguments()
+    steps_Run, Xcol, Ycol, filename, be, rmsd, colModifier = parseArguments()
     # VARIABLES TO SET WHEN PRINTING
     if be:
         kind_Print = "PRINT_BE_RMSD"
     elif rmsd:
         kind_Print = "PRINT_RMSD_STEPS"
 
-    printLine = generatePrintString(steps_Run, Xcol, Ycol, filename, kind_Print)
-    print printLine
+    printLine = generatePrintString(steps_Run, Xcol, Ycol, filename, kind_Print, colModifier)
+    print(printLine)
