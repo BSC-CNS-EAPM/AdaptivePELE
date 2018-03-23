4bf4f14eb452e7979a6d7db885ebc0feb9991f91
jgilaber <joan.gilabert@bsc.es>
2018-03-23 17:59:22 +0100

    Fix minor bug in writeClusteringStructures script

--- AdaptivePELE/analysis/writeClusteringStructures.py
+++ AdaptivePELE/analysis/writeClusteringStructures.py
@@ -17,7 +17,7 @@ def parseArgs():
     parser.add_argument('outputPath', type=str,
                         help="Path where to write the structures, including "
                         "name of the files, i.e output/path/cluster.pdb")
-    parser.add_argument("structures", nargs='*', type=list, default=None,
+    parser.add_argument("structures", nargs='*', type=int, default=None,
                         help="Structures to write")
     parser.add_argument("--threshold", type=float, default=None,
                         help="Only print those structures with mathcing threshold")
