708517d42baf05f4b2ee29afabc53d658492c43b
Dani Soler <daniel.soler@e-campus.uab.cat>
2018-06-08 20:27:23 +0200

    Add folderTraj to basename to identify trajectories correctly

--- AdaptivePELE/freeEnergies/ownBuildMSM.py
+++ AdaptivePELE/freeEnergies/ownBuildMSM.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 import json
 import sys
@@ -40,7 +41,7 @@ def main(control_file):
 
     # parameters
     trajectoryFolder, trajectoryBasename, numClusters, stride, lagtimes, _, _, numberOfITS, _, _, lagtime, clusterCountsThreshold = readParams(control_file)
-
+    trajectoryBasename = os.path.join(trajectoryFolder, trajectoryBasename)
     # program
     clusteringObject = cluster.Cluster(numClusters, trajectoryFolder, trajectoryBasename, alwaysCluster=False, stride=stride)
     clusteringObject.clusterTrajectories()
