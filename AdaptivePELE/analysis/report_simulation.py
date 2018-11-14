af2ca8632a5f426e52d464b3b2b32e1f8fd98321
daniel <daniel.soler@e-campus.uab.cat>
2018-11-14 16:31:23 +0100

    Build automatic layout for pdf report

--- AdaptivePELE/analysis/report_simulation.py
+++ AdaptivePELE/analysis/report_simulation.py
@@ -43,14 +43,14 @@ def write_report(metrics, resname, initial_column=4):
     pdf.cell(30, 10, 'Simulation Report', align='C')
 
     # Plot Binding SASA
-    plot(1+metrics.index("bindingEnergy")+initial_column, 1+metrics.index("sasa")+initial_column, ".", "BE.png")
+    plot(1+metrics.index("bindingEnergy")+initial_column, 1+metrics.index("sasa")+initial_column, ".", "BE.png", "BindingE", "sasa")
     pdf.cell(-100)
     pdf.set_font('Arial', 'B', 11)
     pdf.cell(10, 49, 'Interaction Energy vs Sasa' +34*"\t" + "Total Energy vs Interaction Energy")
     pdf.image("BE.png", 10, 40, 83)
 
     # Plot Total Biding
-    plot(initial_column,  1+metrics.index("sasa")+initial_column, ".", "total.png")
+    plot(initial_column,  1+metrics.index("bindingEnergy")+initial_column, ".", "total.png", "totalE", "BindingE")
     pdf.cell(0)
     pdf.set_font('Arial', 'B', 11)
     pdf.image("total.png", 100, 40, 83)
@@ -62,43 +62,59 @@ def write_report(metrics, resname, initial_column=4):
     pdf.image("{}_threshold.png".format(CONTACTS), 10, 110, 83)
     pdf.image("{}_hist.png".format(CONTACTS), 100, 110, 83)
     
-    #Plot other metrics agains binding
-    doc_position = [-120, 1000]
-    for user_metric in ["rmsd", "com_distance", "distanceToPoint"]:
-        if user_metric in metrics:
-            pdf,doc_position= write_metric(pdf, user_metric, "bindingEnergy", metrics, doc_position)
 
-    #Plot kmean clustering with BE and SASA
-    plot_clusters("bindingEnergy", "sasa", metrics, resname)
-    pdf.cell(0)
-    pdf.cell(doc_position[0], doc_position[1])
-    pdf.image("Cluster_analisis/ClusterMap.png", 100, 110, 83)
+    pdf.add_page()
+
+    #Plot other metrics agains binding
+    images_per_page = 1
+    images_per_row = 0
+    for user_metric in ["rmsd", "com_distance", "distanceToPoint", "clusters"]:
+        if images_per_page > 4:
+            pdf.add_page()
+            images_per_page = 1
+        if user_metric in metrics or user_metric == "clusters":
+            pdf, images_per_page, images_per_row = write_metric(
+	        pdf, user_metric, "bindingEnergy", metrics, images_per_page, images_per_row, resname)
 
     #Output report    
     pdf.output(OUTPUT, 'F')
 
 
 def plot_clusters(metric1, metric2, metrics, resname):
-    print(resname)
     command = command = "python -m AdaptivePELE.analysis.clusterAdaptiveRun 200 {} {} {} --png".format(get_column(metric1, metrics), get_column(metric2, metrics), resname)
-    print(command)
     os.system(command)
 
 def get_column(metric, metrics, initial_pos=4):
     return 1+metrics.index(metric)+initial_pos
 
-def write_metric(pdf, metric1, metric2, metrics, doc_position, initial_pos=4):
-    image_name = "{}.png".format(metric1)
-    pdf.set_font('Arial', 'B', 11)
-    pdf.cell(0)
-    pdf.cell(doc_position[0], doc_position[1])
-    pdf.cell(8, 10, '{} vs {}'.format(metric1, metric2))
-    plot(1+metrics.index(metric1)+initial_pos,  1+metrics.index(metric2)+initial_pos, ".", image_name, zcol=initial_pos + 1+ metrics.index("sasa"))
-    pdf.image(image_name, 65, 20, 83)
-    return pdf, doc_position
+def write_metric(pdf, metric1, metric2, metrics, images_per_page, images_per_row, resname=None, initial_pos=4):
+
+    #Create Image
+    if metric1 == "clusters":
+        image_name = "Cluster_analisis/ClusterMap.png".format(metric1)
+        plot_clusters("bindingEnergy", "sasa", metrics, resname)
+    else:
+        image_name = "{}.png".format(metric1)
+        plot(1+metrics.index(metric1)+initial_pos,  1+metrics.index(metric2)+initial_pos, ".", image_name, metric1, metric2, zcol=initial_pos + 1+ metrics.index("sasa"))
+
+    #Move over pdf
+    pdf.image(image_name, pdf.get_x(), pdf.get_y(), 83)
+    images_per_row += 1
+    if images_per_row == 1:
+        pdf.cell(80, 0, "", 0, 0)
+    else:
+        pdf.set_xy(10, 100)
+        pdf.cell(80, 0, "", 0, 2);
+        images_per_row = 0
+    images_per_page += 1
     
-def plot(Xcol, Ycol, path, name, zcol=5):
-    command = '''gnuplot -e "set terminal png; set output '{}';{}"'''.format(name, plotAdaptive.generatePrintString(8, Xcol, Ycol, "report_", "PRINT_BE_RMSD", zcol, None).strip("\n"))
+    #Update cursor
+    return pdf, images_per_page, images_per_row
+    
+def plot(Xcol, Ycol, path, name, xcol_name, ycol_name, zcol=5):
+    plot_line = plotAdaptive.generatePrintString(8, Xcol, Ycol, "report_", "PRINT_BE_RMSD", zcol, None).strip("\n")
+    command = '''gnuplot -e "set terminal png; set output '{}'; set xlabel '{}'; set ylabel '{}'; {}"'''.format(
+        name, xcol_name, ycol_name, plot_line)
     os.system(command)
         
 def create_contact_plot(path, filenames=CONTACTS):
