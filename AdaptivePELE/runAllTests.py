8457f96bf0c6365b3d60d9aa78ad19ebbc7558a7
jgilaber <joan.gilabert@bsc.es>
2018-11-14 11:23:28 +0100

    Move reporters tests to their own module
    Also create a test for restart with xtc

--- AdaptivePELE/runAllTests.py
+++ AdaptivePELE/runAllTests.py
@@ -9,6 +9,7 @@ from AdaptivePELE.tests import testThresholdcalculator as tThreshold
 from AdaptivePELE.tests import testDensityCalculator as tDensity
 from AdaptivePELE.tests import testMD as tMD
 from AdaptivePELE.tests import testMD_CUDA as tMD_CUDA
+from AdaptivePELE.tests import testReporter as tR
 
 
 def parse_args():
@@ -16,7 +17,7 @@ def parse_args():
             "at -- Run atomset tests\ns  -- Run spawning tests\nth -- Run threshold "
             "calculator tests\nd  -- Run density tests\nc  -- Run clustering tests\n"
             "Ad -- Run adaptive integration tests\nMD -- Run adaptive MD tests\nMD_CUDA"
-            " -- Run adaptive MD tests with CUDA\n")
+            " -- Run adaptive MD tests with CUDA\nR -- Run reporter tests\n")
     parser = argparse.ArgumentParser(description=desc, formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("--run", default=None, nargs="*", help="Tests to run")
     parser.add_argument("--exclude", default=[], nargs="*", help="Tests to exclude")
@@ -28,7 +29,7 @@ def parse_args():
 def main(run, exclude):
     testSuite = unittest.TestSuite()
     if run is None:
-        run = ["at", "s", "th", "d", "c", "Ad", "MD", "MD_CUDA"]
+        run = ["at", "s", "th", "d", "c", "Ad", "MD", "MD_CUDA", "R"]
     to_run = set(run)-set(exclude)
 
     if "at" in to_run or "a" in to_run:
@@ -55,6 +56,9 @@ def main(run, exclude):
     if "MD_CUDA" in to_run or "a" in to_run:
         print("Will run integration tests with md in CUDA")
         testSuite.addTest(unittest.makeSuite(tMD_CUDA.TestMD_CUDA))
+    if "R" in to_run or "a" in to_run:
+        print("Will run repoter tests for OpenMM")
+        testSuite.addTest(unittest.makeSuite(tR.TestReporter))
 
     runner = unittest.TextTestRunner()
     runner.run(testSuite)
