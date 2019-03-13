86d5e7beb21ddbcdf5bcc972b702e1c9f2615d91
jgilaber <joan.gilabert@bsc.es>
2019-03-13 16:41:14 +0100

    Make generateBox more robust

--- AdaptivePELE/analysis/generateBox.py
+++ AdaptivePELE/analysis/generateBox.py
@@ -20,6 +20,21 @@ def parse_arguments():
     return args.top, args.bottom, args.radius, args.title, args.output, args.n_points
 
 
+def generate_perpendicular_vector(input_vec):
+    dim = len(input_vec)
+    # generate two random coordinates
+    p = np.random.rand(dim-1)
+    # select a non-zero component in the input vector
+    ind = np.where(input_vec)[0][0]
+    random_indices = list(range(dim))
+    random_indices.remove(ind)
+    new_vec = np.zeros_like(input_vec)
+    new_vec[ind] = -input_vec[random_indices].dot(p)/input_vec[ind]
+    new_vec[random_indices] = p
+    new_vec /= np.linalg.norm(new_vec)
+    return new_vec
+
+
 def main(top, bot, radius, title="box.pdb", output="", n_points=10):
     if output:
         if not os.path.exists(output):
@@ -30,10 +45,7 @@ def main(top, bot, radius, title="box.pdb", output="", n_points=10):
     l = np.linalg.norm(top-bot)
     alpha = top-bot
     alpha /= np.linalg.norm(alpha)
-    xp, yp = np.random.rand(2)
-    alpha_rot = np.array([xp, yp, -(alpha[0]*xp+alpha[1]*yp)/alpha[2]])
-    alpha_rot /= np.linalg.norm(alpha_rot)
-    alpha_rot *= radius
+    alpha_rot = generate_perpendicular_vector(alpha)*radius
     angle = 2*np.pi/n_points
     rot_mat = utilities.generateRotationMatrixAroundAxis(alpha, angle)
     rotated_vectors = []
