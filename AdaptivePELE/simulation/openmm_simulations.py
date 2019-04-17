8719375e949c7dd77291401c57656c0eeeb506c3
jgilaber <joan.gilabert@bsc.es>
2019-04-17 14:59:27 +0200

    Add enforcePeriodicBox option to xtc reporter

--- AdaptivePELE/simulation/openmm_simulations.py
+++ AdaptivePELE/simulation/openmm_simulations.py
@@ -86,7 +86,7 @@ class XTCReporter(_BaseReporter):
     def backend(self):
         return XTCTrajectoryFile
 
-    def __init__(self, file, reportInterval, atomSubset=None, append=False):
+    def __init__(self, file, reportInterval, atomSubset=None, append=False, enforcePeriodicBox=True):
         if append:
             if isinstance(file, basestring):
                 with self.backend(file, 'r') as f:
@@ -97,6 +97,7 @@ class XTCReporter(_BaseReporter):
                 raise TypeError("I don't know how to handle %s" % file)
         super(XTCReporter, self).__init__(file, reportInterval, coordinates=True, time=True, cell=True, potentialEnergy=False,
                                           kineticEnergy=False, temperature=False, velocities=False, atomSubset=atomSubset)
+        self._enforcePeriodicBox = enforcePeriodicBox
         if append:
             self._traj_file.write(*contents)
 
