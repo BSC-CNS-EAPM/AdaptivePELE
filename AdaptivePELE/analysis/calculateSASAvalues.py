3d90613ebae5299981cbdd3fdaa6b9740dd62cf3
jgilaber <joan.gilabert@bsc.es>
2019-05-03 16:50:06 +0200

    Refactor and parallelize correctRMSD script
    Some functions common with calculateSASAvalues have been moved to the
    analysis_utils module for better resuse

--- AdaptivePELE/analysis/calculateSASAvalues.py
+++ AdaptivePELE/analysis/calculateSASAvalues.py
@@ -7,7 +7,7 @@ import numpy as np
 import mdtraj as md
 import multiprocessing as mp
 from AdaptivePELE.utilities import utilities
-from AdaptivePELE.analysis import correctRMSD
+from AdaptivePELE.analysis import analysis_utils
 
 
 def parseArguments():
@@ -86,27 +86,6 @@ def process_file(traj, top_file, resname, report, outputFilename, format_out, ne
     print("Took %.2fs to process" % (end-start), traj)
 
 
-def process_folder(epoch, folder, trajName, reportName, output_filename, top):
-    if epoch is None:
-        allTrajs = glob.glob(os.path.join(folder, trajName))
-        full_reportName = os.path.join(folder, reportName)
-    else:
-        allTrajs = glob.glob(os.path.join(folder, epoch, trajName))
-        full_reportName = os.path.join(folder, epoch, reportName)
-        epoch = int(epoch)
-
-    allFiles = []
-    for traj in allTrajs:
-        trajNum = utilities.getTrajNum(traj)
-        if top is not None:
-            top_file = top.getTopologyFile(epoch, trajNum)
-        else:
-            top_file = None
-        report_file = full_reportName % trajNum
-        allFiles.append((traj, report_file, top_file, epoch, output_filename % trajNum))
-    return allFiles
-
-
 def main(resname, folder, top, out_report_name, format_out, nProcessors, output_folder, new_report):
     """
         Calculate the relative SASA values of the ligand
@@ -148,10 +127,10 @@ def main(resname, folder, top, out_report_name, format_out, nProcessors, output_
     if not epochs:
         # path does not contain an adaptive simulation, we'll try to retrieve
         # trajectories from the specified path
-        files = process_folder(None, folder, trajName, reportName, os.path.join(folder, outputFilename), top_obj)
+        files = analysis_utils.process_folder(None, folder, trajName, reportName, os.path.join(folder, outputFilename), top_obj)
     for epoch in epochs:
         print("Epoch", epoch)
-        files.extend(process_folder(epoch, folder, trajName, reportName, os.path.join(folder, epoch, outputFilename), top_obj))
+        files.extend(analysis_utils.process_folder(epoch, folder, trajName, reportName, os.path.join(folder, epoch, outputFilename), top_obj))
     results = []
     for info in files:
         results.append(pool.apply_async(process_file, args=(info[0], info[2], resname, info[1], info[4], format_out, new_report, info[3])))
