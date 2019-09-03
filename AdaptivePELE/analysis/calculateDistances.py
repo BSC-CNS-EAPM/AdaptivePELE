02cee348aea11606c4e8f0ec1a0050e87dc6c359
jgilaber <joan.gilabert@bsc.es>
2019-09-03 16:18:04 +0200

    Add calculateDistances.py script

--- AdaptivePELE/analysis/calculateDistances.py
+++ AdaptivePELE/analysis/calculateDistances.py
@@ -18,7 +18,7 @@ def parseArguments():
     """
     desc = "Program that caculates the relative SASA of a ligand."
     parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument("resname", type=str, help="Ligand resname")
+    parser.add_argument("residues", type=str, nargs='+', help="Atom pairs to calculate the distances in the format res_name1:res_number1:atomname1-res_name2:res_number2:atomname2")
     parser.add_argument("--path", type=str, default=".", help="Path where the simulation is stored")
     parser.add_argument("--top", type=str, default=None, help="Topology file for non-pdb trajectories or path to Adaptive topology object")
     parser.add_argument("--out_name", type=str, default="fixedReport", help="Name of the modified report files (default is fixedReport)")
@@ -29,40 +29,44 @@ def parseArguments():
     parser.add_argument("--traj_to_process", nargs="*", type=int, default=None, help="Number of the trajectories to filter, if not specified all of them will be processed")
     args = parser.parse_args()
 
-    return args.resname, args.path, args.top, args.out_name, args.fmt_str, args.n, args.out_folder, args.new_report, args.traj_to_process
+    return args.residues, args.path, args.top, args.out_name, args.fmt_str, args.n, args.out_folder, args.new_report, args.traj_to_process
 
 
-def calculateSASA(trajectory, topology, res_name):
+def parse_selection(res_input):
+    parsed_selection = []
+    for residues in res_input:
+        res_info = residues.split("-")
+        parsed_selection.append(tuple([tuple(res.split(":")) for res in res_info]))
+    return parsed_selection
+
+
+def calculate_distances(trajectory, topology, residues):
     """
-        Calculate the SASA of a ligand in a trajectory
+        Calculate the distances between pairs of atoms in a trajectory
 
         :param trajectory: Name of the trajectory file
         :type trajectory: str
         :param topology: Topology of the trajectory (needed for non-pdb trajs)
         :type topology: str
-        :param res_name: Ligand resname
-        :type res_name: str
+        :param residues: Pairs of atoms to calculate distances
+        :type residues: list
     """
     utilities.print_unbuffered("Processing", trajectory)
     t = md.load(trajectory, top=topology)
-    t.remove_solvent(inplace=True)
-    res_atoms = t.top.select("resname '%s'" % res_name)
-    if not len(res_atoms):
-        raise ValueError("Nothing found using resname %s" % res_name)
-    t2 = t.atom_slice(res_atoms)
-    for atom in t2.top.atoms:
-        # mdtraj complains if the ligand residue index is not 0 when
-        # isolated
-        atom.residue.index = 0
-    res_index = t.top.atom(res_atoms[0]).residue.index
-    sasa = md.shrake_rupley(t, mode="residue")
-    sasa_empty = md.shrake_rupley(t2, mode="residue")
-    return sasa[:, res_index]/sasa_empty[:, 0]
+    atom_pairs = []
+    for info1, info2 in residues:
+        atom1 = t.top.select("resname '%s' and residue %s and name %s" % info1)
+        atom2 = t.top.select("resname '%s' and residue %s and name %s" % info2)
+        if atom1.size == 0 or atom2.size == 0:
+            raise ValueError("Nothing found under current selection")
+        atom_pairs.append(atom1.tolist()+atom2.tolist())
+    atom_pairs = np.array(atom_pairs)
+    return 10*md.compute_distances(t, atom_pairs)
 
 
-def process_file(traj, top_file, resname, report, outputFilename, format_out, new_report, epoch):
+def process_file(traj, top_file, residues, report, outputFilename, format_out, new_report, epoch, distances_header):
     start = time.time()
-    sasa_values = calculateSASA(traj, top_file, resname)
+    distances = calculate_distances(traj, top_file, residues)
     header = ""
     if not new_report:
         try:
@@ -71,34 +75,32 @@ def process_file(traj, top_file, resname, report, outputFilename, format_out, ne
             raise IndexError("File %s not found" % report)
         if outputFilename != reportFilename:
             reportFilename = outputFilename
-
         with open(reportFilename) as f:
             header = f.readline().rstrip()
             if not header.startswith("#"):
                 header = ""
             reportFile = utilities.loadtxtfile(f)
 
-        fixedReport = analysis_utils.extendReportWithRmsd(reportFile, sasa_values)
+        fixedReport = analysis_utils.extendReportWithRmsd(reportFile, distances)
     else:
-        indexes = np.array(range(sasa_values.shape[0]))
-        fixedReport = np.concatenate((indexes[:, None], sasa_values[:, None]), axis=1)
-
+        indexes = np.array(range(distances.shape[0]))
+        fixedReport = np.concatenate((indexes[:, None], distances), axis=1)
     with open(outputFilename, "w") as fw:
         if header:
-            fw.write("%s\tSASA\n" % header)
+            fw.write("%s\t%s\n" % (header, distances_header))
         else:
-            fw.write("# Step\tSASA\n")
+            fw.write("# Step\t%s\n" % distances_header)
         np.savetxt(fw, fixedReport, fmt=format_out, delimiter="\t")
     end = time.time()
     print("Took %.2fs to process" % (end-start), traj)
 
 
-def main(resname, folder, top, out_report_name, format_out, nProcessors, output_folder, new_report, trajs_to_select):
+def main(residues, folder, top, out_report_name, format_out, nProcessors, output_folder, new_report, trajs_to_select):
     """
-        Calculate the relative SASA values of the ligand
+        Calculate the distances between paris of atoms
 
-        :param resname: Ligand resname
-        :type resname: str
+        :param residues: Pairs of atoms to calculate distances
+        :type residues: list
         :param folder: Path the simulation
         :type folder: str
         :param top: Path to the topology
@@ -122,10 +124,12 @@ def main(resname, folder, top, out_report_name, format_out, nProcessors, output_
     outputFilename = "_".join([out_report_name, "%d"])
     trajName = "*traj*"
     reportName = "*report*_%d"
+    distances_label = "\t".join(residues)
+    residues = parse_selection(residues)
     if nProcessors is None:
         nProcessors = utilities.getCpuCount()
     nProcessors = max(1, nProcessors)
-    print("Calculating SASA with %d processors" % nProcessors)
+    print("Calculating distances with %d processors" % nProcessors)
     epochs = utilities.get_epoch_folders(folder)
     if top is not None:
         top_obj = utilities.getTopologyObject(top)
@@ -141,14 +145,14 @@ def main(resname, folder, top, out_report_name, format_out, nProcessors, output_
         files.extend(analysis_utils.process_folder(epoch, folder, trajName, reportName, os.path.join(folder, epoch, outputFilename), top_obj, trajs_to_select))
     print("Starting to process files!")
     pool = mp.Pool(nProcessors)
-    results = [pool.apply_async(process_file, args=(info[0], info[2], resname, info[1], info[4], format_out, new_report, info[3])) for info in files]
+    results = [pool.apply_async(process_file, args=(info[0], info[2], residues, info[1], info[4], format_out, new_report, info[3], distances_label)) for info in files]
     pool.close()
     pool.join()
     for res in results:
         res.get()
 
 if __name__ == "__main__":
-    lig_name, path, topology_path, out_name, fmt_str, n_proc, out_folder, new_reports, traj_filter = parseArguments()
+    pairs, path, topology_path, out_name, fmt_str, n_proc, out_folder, new_reports, traj_filter = parseArguments()
     if traj_filter is not None:
         traj_filter = set(traj_filter)
-    main(lig_name, path, topology_path, out_name, fmt_str, n_proc, out_folder, new_reports, traj_filter)
+    main(pairs, path, topology_path, out_name, fmt_str, n_proc, out_folder, new_reports, traj_filter)
