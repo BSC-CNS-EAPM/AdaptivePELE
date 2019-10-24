e22c54a203f5c721ac30d9d48ba8ef38e05ec38e
jgilaber <joan.gilabert@bsc.es>
2019-10-24 11:08:48 +0200

    Add test with multiple initial structures for MD

--- AdaptivePELE/tests/testMD_CUDA.py
+++ AdaptivePELE/tests/testMD_CUDA.py
@@ -51,3 +51,15 @@ class TestMD_CUDA(unittest.TestCase):
         time.sleep(10)
         # cleanup
         utilities.cleanup(output_path)
+
+    def testOpenMM3ptb_2replicas_multiple_initial_CUDA(self):
+        output_path = "tests/data/openmm_3ptb_2replica_multiple_initial_CUDA"
+        controlFile = "tests/data/templetized_controlFile_3ptb_CUDA_2replicas_multple_initials_md.conf"
+
+        adaptiveSampling.main(controlFile)
+        self.check_succesful_simulation(output_path, 2, 4)
+        # ensure all replicas have enough time to check the output before
+        # deleting it
+        time.sleep(10)
+        # cleanup
+        utilities.cleanup(output_path)
