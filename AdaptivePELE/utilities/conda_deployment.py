c1c50fcabac347523bad0736ccac172df174f4c2
Daniel Soler Viladrich <daniel.soler@e-campus.uab.cat>
2019-11-13 15:12:54 +0100

    Update conda deployment

---
+++ AdaptivePELE/utilities/conda_deployment.py
@@ -0,0 +1,9 @@
+import os
+
+PYTHONS = ["3.7", "3.6", "3.5", "2.7"]
+
+for python in PYTHONS:
+    print("conda build -c conda-forge conda_recipe/ --python={}".format(python))
+    os.system("conda build -c conda-forge conda_recipe/ --python={}".format(python))
+        
+    
